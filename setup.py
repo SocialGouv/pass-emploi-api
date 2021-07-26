2588364adbd5c93876d6e3dbf4c47ecd6e2ec444
Ahmed <aalaoui@octo.com>
2021-07-26 12:05:03 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pass-emploi-api',
+      version='0.0.1',
+      description='Pass Emploi API',
+      url='https://github.com/SocialGouv/pass-emploi-api',
+      install_requires=[
+          'flask'
+      ],
+      zip_safe=False)
