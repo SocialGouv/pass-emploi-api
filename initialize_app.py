16cb0c70f4017385a381ac459f53a54db32b3b55
@gabriel_adgeg <gabriel.adgeg.ext@francetv.fr>
2021-09-03 20:05:16 +0200

    [DB] Log database env variable error

--- initialize_app.py
+++ initialize_app.py
@@ -9,7 +9,5 @@ with app.app_context():
     load_dotenv(dotenv_path='./.env')
     environment = os.environ.get('ENV')
     IS_DEV = environment == 'development'
-    database_url = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    print("DATABASE URL: " + database_url)
-    if database_url is None:
-        app.logger.warning('Database URL: %s', database_url)
+    if os.environ.get('SQLALCHEMY_DATABASE_URI') is None:
+        app.logger.error('Warning: database URL is not set')
