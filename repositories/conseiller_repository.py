84dd83cc781bd36a87ec445421b3ddd64845a976
@gabriel_adgeg <gabriel.adgeg.ext@francetv.fr>
2021-09-03 20:01:41 +0200

    [DB] BSR

--- repositories/conseiller_repository.py
+++ repositories/conseiller_repository.py
@@ -3,19 +3,17 @@ from datasources.jeune_database_datasource import JeuneDatabaseDatasource
 from model.conseiller import Conseiller
 from model.jeune import Jeune
 from transformers.conseiller_transformer import to_conseiller
+from transformers.jeune_transformer import to_jeune
 
 
 class ConseillerRepository:
-
     def __init__(self, conseiller_datasource: ConseillerDatabaseDatasource, jeune_datasource: JeuneDatabaseDatasource):
         self.conseillerDatasource = conseiller_datasource
         self.jeuneDatasource = jeune_datasource
 
     def get_jeunes(self, conseiller: Conseiller) -> [Jeune]:
         sql_jeunes_list = self.jeuneDatasource.get_jeunes_list(int(conseiller.id))
-        return list(map(lambda j: Jeune(j.id, j.firstName, j.lastName,
-                                        Conseiller(str(j.conseiller.id), j.conseiller.firstName,
-                                                   j.conseiller.lastName)), sql_jeunes_list))
+        return list(map(lambda sql_jeune: to_jeune(sql_jeune), sql_jeunes_list))
 
     def get_random_conseiller(self) -> Conseiller:
         return to_conseiller(self.conseillerDatasource.get_random_conseiller())
