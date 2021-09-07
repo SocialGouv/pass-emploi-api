21c827b91b776785c64f265ff877b62a42aee8f4
@gabriel_adgeg <gabriel.adgeg.ext@francetv.fr>
2021-09-07 14:58:47 +0200

    [DB] Add additionnal logs

--- initialize_db.py
+++ initialize_db.py
@@ -8,14 +8,20 @@ from initialize_app import app
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.logger.warn('Initialize SQLAlchemy db...')
+app.logger.warn('>>> Initialize SQLAlchemy db...')
 db = SQLAlchemy(app)
-app.logger.warn('Initialize SQLAlchemy done')
+app.logger.warn('>>> Initialize SQLAlchemy done')
 
 
 def run_migrations() -> None:
-    app.logger.warn('>>> Inside run_migrations...')
+    app.logger.warn('>>>>>> Inside run_migrations...')
     alembic_cfg = Config('alembic.ini')
+    app.logger.warn('>>>>>> After alembic_cfg = Config(alembic.ini)')
     alembic_cfg.set_main_option("sqlalchemy.url", app.config['SQLALCHEMY_DATABASE_URI'])
-    command.upgrade(alembic_cfg, 'head')
-    app.logger.warn('>>> Inside run_migrations done')
+    app.logger.warn('>>>>>> After set_main_option')
+    try:
+        command.upgrade(alembic_cfg, 'head')
+        app.logger.warn('>>>>>> After command.upgrade')
+    except:
+        app.logger.error('>>>>>> command.upgrade done')
+    app.logger.warn('>>>>>> Inside run_migrations done')
