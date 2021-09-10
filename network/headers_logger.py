6daee30e0e0b750247e33bf8e254651db21e9433
@gabriel_adgeg <gabriel.adgeg.ext@francetv.fr>
2021-09-10 15:40:50 +0200

    [BSR]

--- network/headers_logger.py
+++ network/headers_logger.py
@@ -5,12 +5,13 @@ from initialize_app import app
 SEPARATOR = ', '
 
 
-def log_headers():
+def log_headers() -> None:
     message = get_header_log_if_exists('X-AppVersion') \
               + get_header_log_if_exists('X-Platform') \
               + get_header_log_if_exists('X-InstallationId') \
               + get_header_log_if_exists('X-CorrelationId')
-    app.logger.info(message)
+    if len(message) > 0:
+        app.logger.info(message)
 
 
 def get_header_log_if_exists(key) -> str:
