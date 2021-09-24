907a76b8b3c67a6034b48d4866accf5bd20f1b15
Ahmed <aalaoui@octo.com>
2021-09-24 10:26:54 +0200

    refacto: create sandbox with mocked data for development

---
+++ sandbox/create_sandbox.py
@@ -0,0 +1,39 @@
+from initialize_db import db
+
+
+def create_sandbox() -> None:
+    query = """
+            TRUNCATE TABLE jeune CASCADE;
+            TRUNCATE TABLE conseiller CASCADE;
+            
+            INSERT INTO conseiller (id, first_name, last_name) VALUES ('1', 'Nils', 'Tavernier');
+            
+            INSERT INTO jeune (id, first_name, last_name, conseiller_id) VALUES ('1', 'Kenji', 'Lefameux', '1');
+            
+            INSERT INTO rendezvous (id, title, subtitle, comment, modality, date, duration, jeune_id, conseiller_id)
+                   VALUES ('1', 'Rendez-vous conseiller', 'avec Nils', 'Suivi des actions', 'Par téléphone', 
+                   '2021-09-24 10:00:00', '00:30:00', '1', '1');
+                   
+            INSERT INTO rendezvous (id, title, subtitle, comment, modality, date, duration, jeune_id, conseiller_id)
+                   VALUES ('2', 'Rendez-vous conseiller', 'avec Nils', 'Suivi des actions', 'Par téléphone', 
+                   '2021-09-25 10:00:00', '00:30:00', '1', '1');
+                   
+            INSERT INTO rendezvous (id, title, subtitle, comment, modality, date, duration, jeune_id, conseiller_id)
+                   VALUES ('3', 'Rendez-vous conseiller', 'avec Nils', 'Suivi des actions', 'Par téléphone', 
+                   '2021-09-26 10:00:00', '00:30:00', '1', '1');
+                   
+            
+            INSERT INTO action (id, content, comment, is_done, creation_date, last_update, jeune_id) VALUES
+                    ('1', 'Suivre une formation', 'Consulter le catalogue des formations', 'false', 
+                    '2021-09-25 10:00:00', '2021-09-24 10:00:00', '1');
+                    
+            INSERT INTO action (id, content, comment, is_done, creation_date, last_update, jeune_id) VALUES
+                    ('2', 'Suivre une formation', 'Consulter le catalogue des formations', 'false', 
+                    '2021-09-25 10:00:00', '2021-09-24 10:00:00', '1');
+                    
+            INSERT INTO action (id, content, comment, is_done, creation_date, last_update, jeune_id) VALUES
+                    ('3', 'Suivre une formation', 'Consulter le catalogue des formations', 'false', 
+                    '2021-09-26 10:00:00', '2021-09-24 10:00:00', '1');              
+        """
+    db.engine.execute(query)
+    db.session.commit()
