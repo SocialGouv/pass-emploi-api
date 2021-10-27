a388f28554dfe414485978ce74d9f854b48e19fc
Malek Zemni <3781318-Mzem@users.noreply.gitlab.com>
2021-10-27 16:23:40 +0200

    feat: add OffreEmploi class

--- repositories/offres_emploi_repository.py
+++ repositories/offres_emploi_repository.py
@@ -1,4 +1,5 @@
 from datasources.offres_emploi_api_datasource import OffresEmploiAPIDatasource
+from model.offre_emploi import OffreEmploi
 from transformers.offres_emploi_transformer import to_offres_emploi
 
 
@@ -7,6 +8,6 @@ class OffresEmploiRepository:
     def __init__(self, offres_emploi_datasource: OffresEmploiAPIDatasource):
         self.offresEmploiDataSource = offres_emploi_datasource
 
-    def get_offres_emploi(self):
-        offres_emploi_api_response = self.offresEmploiDataSource.get_offres_emploi()
-        return to_offres_emploi(offres_emploi_api_response)
+    def get_offres_emploi(self) -> [OffreEmploi]:
+        offres_emploi_response = self.offresEmploiDataSource.get_offres_emploi()
+        return to_offres_emploi(offres_emploi_response)
--- transformers/offres_emploi_transformer.py
+++ transformers/offres_emploi_transformer.py
@@ -1,2 +1,6 @@
-def to_offres_emploi(offres_emploi_api_response: dict):
-    return offres_emploi_api_response
+from model.offre_emploi import OffreEmploi
+
+
+def to_offres_emploi(offres_emploi_response: dict) -> [OffreEmploi]:
+    results = offres_emploi_response['resultats']
+    return [OffreEmploi(offre['intitule'], offre['description']) for offre in results]
