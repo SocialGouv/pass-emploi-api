c18d4df7d74b3ee23ec605eb7182cd4df5a090ba
Ahmed <aalaoui@octo.com>
2021-10-28 17:35:46 +0200

    refacto: remove health check route to separate folder and initialize routes separately

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
 from waitress import serve
 
-from initialize_app import app, DEBUG
+from config import DEBUG
+from initialize_app import app
 from initialize_db import run_migrations
+from initialize_routes import initialize_routes
 from sandbox.create_sandbox import create_sandbox
-from infrastructure.routes.common_routes import common_routes
-from infrastructure.routes.mobile import mobile
-from infrastructure.routes.web import web
-from infrastructure.routes.offres_emploi import offres_emploi
 
 run_migrations()
 
 if DEBUG:
     create_sandbox()
 
-app.register_blueprint(web)
-app.register_blueprint(mobile)
-app.register_blueprint(common_routes)
-app.register_blueprint(offres_emploi)
-
-
-@app.route('/')
-def health_check():
-    return 'Pass Emploi version bêta.'
+initialize_routes(app)
 
 
 if __name__ == '__main__':
