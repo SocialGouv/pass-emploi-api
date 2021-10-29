41e49012b6c41613194730ebcfa2c5330a4732bd
Malek Zemni <3781318-Mzem@users.noreply.gitlab.com>
2021-10-29 15:33:55 +0200

    fix: gad reviews + clean

--- datasources/offres_emploi_api_datasource.py
+++ datasources/offres_emploi_api_datasource.py
@@ -1,5 +1,9 @@
+from typing import Optional
+
 from infrastructure.services.pole_emploi.pole_emploi_api_initializer import PoleEmploiApiInitializer
 from infrastructure.services.utils.get_response_json import get_response_json
+
+OFFRES_EMPLOI_SUFFIX_URL = 'offresdemploi/v2/offres/search'
 
 
 class OffresEmploiAPIDatasource:
@@ -7,7 +11,6 @@ class OffresEmploiAPIDatasource:
     def __init__(self, pole_emploi_api_initializer: PoleEmploiApiInitializer):
         self.apiInitializer = pole_emploi_api_initializer
 
-    def get_offres_emploi(self):
-        SUFFIX_URL = 'offresdemploi/v2/offres/search'
-        res = self.apiInitializer.init_api().get(SUFFIX_URL)
-        return get_response_json(res)
+    def get_offres_emploi(self) -> Optional[dict]:
+        response = self.apiInitializer.init_api().get(OFFRES_EMPLOI_SUFFIX_URL)
+        return get_response_json(response)
--- infrastructure/services/cache/redis_client.py
+++ infrastructure/services/cache/redis_client.py
@@ -1,26 +1,23 @@
 import os
 import redis
 
-url = os.environ.get('REDIS_URL')
-is_enabled = url and (url is not '')
+URL = os.environ.get('REDIS_URL')
+IS_ENABLED = URL and (URL is not '')
 
 
 class RedisClient:
 
     def __init__(self):
-        self.client = redis.from_url(url, charset="utf-8", decode_responses=True)
+        self.client = redis.from_url(URL, charset="utf-8", decode_responses=True) if IS_ENABLED else None
 
     def get(self, key: str):
         try:
-            print(f'getting {key} from cache')
             return self.client.get(key)
         except Exception:
             return None
 
     def set(self, key: str, value: str, expiry=None):
         try:
-            print(f'setting {key} : {value} in cache')
-
             return self.client.set(key, value, ex=expiry)
         except Exception:
             return None
