eb769975c6a3036c7ed47407110353ce3328f13d
Ahmed <aalaoui@octo.com>
2021-11-03 12:13:46 +0100

    fix: change duration format when creating rendezvous and test it

--- use_cases/create_rendezvous_request.py
+++ use_cases/create_rendezvous_request.py
@@ -1,5 +1,5 @@
 class CreateRendezvousRequest:
-    def __init__(self, comment: str, date: str, duration: str, modality: str, jeune_id: str, conseiller_id: str):
+    def __init__(self, comment: str, date: str, duration: int, modality: str, jeune_id: str, conseiller_id: str):
         self.comment = comment
         self.date = date
         self.duration = duration
