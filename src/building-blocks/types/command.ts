8b8d15a2d1bda1d95712d033cd59aaaba042d474
Malek Zemni <3781318-Mzem@users.noreply.gitlab.com>
2021-11-29 15:12:16 +0100

    fix: appliquer le formatter sur les anciens fichiers

--- src/building-blocks/types/command.ts
+++ src/building-blocks/types/command.ts
@@ -4,6 +4,5 @@
  * @see https://martinfowler.com/bliki/CommandQuerySeparation.html
  * @see https://udidahan.com/2009/12/09/clarified-cqrs/
  */
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Command {
-}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Command {}
