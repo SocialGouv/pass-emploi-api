c2373f87658c68ea18f641b17b96c06e9dd83b35
Ahmed <aalaoui@octo.com>
2021-12-03 15:22:38 +0100

    refacto: tester le mapper des offres emploi

--- test/fixtures/offre-emploi.fixture.ts
+++ test/fixtures/offre-emploi.fixture.ts
@@ -1,5 +1,6 @@
 import { OffreEmploiResumeQueryModel } from 'src/application/queries/query-models/offres-emploi.query-models'
 import { OffreEmploi } from '../../src/domain/offre-emploi'
+import { OffreEmploiDto } from '../../src/infrastructure/repositories/offre-emploi-http-sql.repository'
 
 export const uneOffreEmploi = (): OffreEmploi => ({
   id: '123DXPM',
@@ -11,6 +12,28 @@ export const uneOffreEmploi = (): OffreEmploi => ({
     nom: '77 - LOGNES',
     codePostal: '77185',
     commune: '77258'
+  },
+  alternance: false
+})
+
+export const uneOffreEmploiDto = (): OffreEmploiDto => ({
+  id: '123DXPM',
+  intitule: 'Technicien / Technicienne en froid et climatisation',
+  typeContrat: 'MIS',
+  dureeTravailLibelleConverti: 'Temps plein',
+  entreprise: {
+    nom: 'RH TT INTERIM'
+  },
+  lieuTravail: {
+    libelle: 'libelle',
+    codePostal: '57000',
+    commune: '57463'
+  },
+  contact: {
+    urlPostulation: 'url/postulation'
+  },
+  origineOffre: {
+    urlOrigine: 'url/offre'
   },
   alternance: false
 })
