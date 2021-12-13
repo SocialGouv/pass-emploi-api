ed1537ce1c240eb23dd032f0124548e4fa82592d
Adrien Graux <adrien.graux@octo.com>
2021-12-13 15:27:26 +0100

    feat: log en json

--- src/infrastructure/routes/referentiels.controller.ts
+++ src/infrastructure/routes/referentiels.controller.ts
@@ -1,25 +1,19 @@
-import { Controller, Get, Logger, Query } from '@nestjs/common'
+import { Controller, Get, Query } from '@nestjs/common'
 import { ApiResponse, ApiTags } from '@nestjs/swagger'
 import {
   GetCommunesEtDepartementsQuery,
   GetCommunesEtDepartementsQueryHandler
 } from '../../application/queries/get-communes-et-departements.query.handler'
 import { CommunesEtDepartementsQueryModel } from '../../application/queries/query-models/communes-et-departements.query-model'
-import { Authentification } from '../../domain/authentification'
-import { Utilisateur } from '../decorators/authenticated.decorator'
 import { Public } from '../decorators/public.decorator'
 
 @Public()
 @Controller('referentiels')
 @ApiTags('Referentiels')
 export class ReferentielsController {
-  private logger: Logger
-
   constructor(
     private readonly getCommunesEtDepartementsQueryHandler: GetCommunesEtDepartementsQueryHandler
-  ) {
-    this.logger = new Logger('ReferentielsController')
-  }
+  ) {}
 
   @Get('communes-et-departements')
   @ApiResponse({
@@ -27,10 +21,8 @@ export class ReferentielsController {
     isArray: true
   })
   async getCommunesEtDepartements(
-    @Query('recherche') recherche: string,
-    @Utilisateur() utilisateur: Authentification.Utilisateur
+    @Query('recherche') recherche: string
   ): Promise<CommunesEtDepartementsQueryModel[]> {
-    this.logger.log(utilisateur)
     const query: GetCommunesEtDepartementsQuery = { recherche }
     const queryModel = await this.getCommunesEtDepartementsQueryHandler.execute(
       query
