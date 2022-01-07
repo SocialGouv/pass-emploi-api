b8a4a52c1bed54e245bda5a6ce38c8933b77e52c
Malek Zemni <3781318-Mzem@users.noreply.gitlab.com>
2022-01-07 11:06:07 +0100

    refacto: wip

--- src/infrastructure/repositories/evenement-http.repository.ts
+++ src/infrastructure/repositories/evenement-http.repository.ts
@@ -1,7 +1,6 @@
 import { HttpService } from '@nestjs/axios'
 import { Injectable, Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-//import { RuntimeException } from '@nestjs/core/errors/exceptions/runtime.exception'
 import { firstValueFrom } from 'rxjs'
 import { Evenement } from 'src/domain/evenement'
 
@@ -20,7 +19,6 @@ export class EvenementHttpRepository implements Evenement.Repository {
 
   async sendEvenement(evenement: Evenement.Evenement): Promise<void> {
     const params = new URLSearchParams()
-    params.append('rec', '1')
     params.append('idsite', this.configService.get('matomo').envId)
 
     const evenementCategorieQueryParam = 'e_c'
@@ -38,17 +36,13 @@ export class EvenementHttpRepository implements Evenement.Repository {
     }
 
     try {
-      const response = await firstValueFrom(
-        this.httpService.post(`${this.apiUrl}`, {
+      await firstValueFrom(
+        this.httpService.get(`${this.apiUrl}`, {
           params
         })
       )
-      this.logger.log('###########################')
-      this.logger.log(JSON.stringify(response))
-      // TODO : @Ahmed tu peux voir comment faire fonctionner la requete à Matomo ?
     } catch (e) {
       this.logger.error(e)
-      //throw new RuntimeException(e.statusText)
     }
   }
 }
