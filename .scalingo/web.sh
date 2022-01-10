1c1a2479d95970275fbd179cb9465f7bff4b966a
Jozo <jozo.robert@gmail.com>
2022-01-10 15:52:04 +0100

    plop

--- .scalingo/web.sh
+++ .scalingo/web.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
 yarn start:prod
-
-if [[ $APP =~ "pa-back-staging-pr" ]] ; then
-  scalingo -a $APP env-set "BASE_URL=plopplop"
-  BASE_URL=plop yarn start:prod
-else
-    yarn start:prod
-fi
