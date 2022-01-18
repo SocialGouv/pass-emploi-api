8a236179a563a5e7a73930c3427f74b93108d34b
Ahmed <aalaoui@octo.com>
2022-01-18 16:25:31 +0100

    refactor: tester le repo et le command handler

---
+++ test/fixtures/offre-immersion.fixture.ts
@@ -0,0 +1,9 @@
+import { OffreImmersion } from '../../src/domain/offre-immersion'
+
+export const uneOffreImmersion = (): OffreImmersion => ({
+  id: '123ABC',
+  nomEtablissement: 'Mécanique du Rhône',
+  metier: 'Mécanicien',
+  ville: 'Lyon',
+  secteurActivite: 'Industrie auto'
+})
