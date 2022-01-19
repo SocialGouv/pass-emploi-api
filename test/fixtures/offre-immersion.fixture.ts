2de8d2d2b52e7881b7810bd57aee9c9562d8494d
sandra.hage.chehade <sandra.hage.chehade@octo.com>
2022-01-19 16:04:38 +0100

    test repository get id et favoris imeersions

--- test/fixtures/offre-immersion.fixture.ts
+++ test/fixtures/offre-immersion.fixture.ts
@@ -1,6 +1,15 @@
+import { OffreImmersionQueryModel } from 'src/application/queries/query-models/offres-immersion.query-models'
 import { OffreImmersion } from '../../src/domain/offre-immersion'
 
 export const uneOffreImmersion = (): OffreImmersion => ({
+  id: '123ABC',
+  nomEtablissement: 'Mécanique du Rhône',
+  metier: 'Mécanicien',
+  ville: 'Lyon',
+  secteurActivite: 'Industrie auto'
+})
+
+export const uneOffreImmersionQueryModel = (): OffreImmersionQueryModel => ({
   id: '123ABC',
   nomEtablissement: 'Mécanique du Rhône',
   metier: 'Mécanicien',
