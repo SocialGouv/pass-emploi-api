a1e0ee3e1c9087d14184c905be15b7fb88d43fe3
Malek Zemni <3781318-Mzem@users.noreply.gitlab.com>
2022-01-31 08:04:22 +0100

    feat: down migration

--- src/infrastructure/sequelize/migrations/20220126170128-supprimer-conseiller-rendezvous.js
+++ src/infrastructure/sequelize/migrations/20220126170128-supprimer-conseiller-rendezvous.js
@@ -26,6 +26,31 @@ module.exports = {
         },
         { transaction }
       )
+      const jeunes = await queryInterface.sequelize.query(
+        `SELECT id, id_conseiller FROM jeune`,
+        { type: Sequelize.QueryTypes.SELECT, transaction }
+      )
+      for (const { id, id_conseiller } of jeunes) {
+        await queryInterface.sequelize.query(
+          `UPDATE rendez_vous SET id_conseiller = :id_conseiller WHERE id_jeune = :id`,
+          {
+            type: Sequelize.QueryTypes.UPDATE,
+            replacements: { id, id_conseiller },
+            transaction
+          }
+        )
+      }
+      await queryInterface.changeColumn(
+        'rendez_vous',
+        'id_conseiller',
+        {
+          type: Sequelize.STRING,
+          allowNull: false,
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        },
+        { transaction }
+      )
     })
   }
 }
