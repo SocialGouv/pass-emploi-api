863f7ad0554e6247e21fd567ef927998d108c97d
Jozo <jozo.robert@gmail.com>
2022-02-18 11:29:34 +0100

    refactor: on tests les pipe

--- test/infrastructure/routes/services-civique.controller.test.ts
+++ test/infrastructure/routes/services-civique.controller.test.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, INestApplication } from '@nestjs/common'
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common'
 import * as request from 'supertest'
 import { unHeaderAuthorization } from '../../fixtures/authentification.fixture'
 import {
@@ -8,11 +8,15 @@ import {
   stubClass
 } from '../../utils'
 import { ensureUserAuthenticationFailsIfInvalid } from '../../utils/ensure-user-authentication-fails-if-invalid'
-import { success } from '../../../src/building-blocks/types/result'
-import { GetServicesCiviqueQueryHandler } from 'src/application/queries/get-services-civique.query.handler'
+import { failure, success } from '../../../src/building-blocks/types/result'
+import {
+  GetServicesCiviqueQuery,
+  GetServicesCiviqueQueryHandler
+} from 'src/application/queries/get-services-civique.query.handler'
 import { OffreEngagementQueryModel } from '../../../src/application/queries/query-models/service-civique.query-models'
 import { DateTime } from 'luxon'
 import { offreEngagementQueryModel } from '../../fixtures/query-models/offre-engagement.query-model.fixtures'
+import { ErreurHttp } from '../../../src/building-blocks/types/domain-error'
 
 describe('ServicesCiviqueController', () => {
   let getServicesCiviqueQueryHandler: StubbedClass<GetServicesCiviqueQueryHandler>
@@ -27,6 +31,7 @@ describe('ServicesCiviqueController', () => {
       .compile()
 
     app = testingModule.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
     await app.init()
   })
 
@@ -35,47 +40,76 @@ describe('ServicesCiviqueController', () => {
   })
 
   describe('GET /services-civique', () => {
-    it("fait appel à l'API services civique avec les bons paramètres", async () => {
-      // Given
-      const findServicesCiviqueQuery = {
-        page: '1',
-        limit: '50',
-        dateDeDebutMaximum: '2022-02-17T10:00:00Z',
-        dateDeDebutMinimum: '2022-02-17T10:00:00Z',
-        lat: '48.86899229710103',
-        lon: '2.3342718577284205',
-        distance: '10',
-        domaine: 'environnement'
-      }
-      const expectedQuery = {
-        page: '1',
-        limit: '50',
-        dateDeDebutMaximum: DateTime.fromISO('2022-02-17T10:00:00Z'),
-        dateDeDebutMinimum: DateTime.fromISO('2022-02-17T10:00:00Z'),
-        lat: '48.86899229710103',
-        lon: '2.3342718577284205',
-        distance: '10',
-        domaine: 'environnement'
-      }
+    describe('quand tout va bien', () => {
+      it("fait appel à l'API services civique avec les bons paramètres", async () => {
+        // Given
+        const findServicesCiviqueQuery = {
+          page: '1',
+          limit: '50',
+          dateDeDebutMaximum: '2022-02-17T10:00:00Z',
+          dateDeDebutMinimum: '2022-02-17T10:00:00Z',
+          lat: '48.86899229710103',
+          lon: '2.3342718577284205',
+          distance: '10',
+          domaine: 'environnement'
+        }
+        const expectedQuery: GetServicesCiviqueQuery = {
+          page: 1,
+          limit: 50,
+          dateDeDebutMaximum: DateTime.fromISO('2022-02-17T10:00:00Z'),
+          dateDeDebutMinimum: DateTime.fromISO('2022-02-17T10:00:00Z'),
+          lat: 48.86899229710103,
+          lon: 2.3342718577284205,
+          distance: 10,
+          domaine: 'environnement'
+        }
 
-      const serviceCiviqueQueryModels: OffreEngagementQueryModel[] =
-        offreEngagementQueryModel()
+        const serviceCiviqueQueryModels: OffreEngagementQueryModel[] =
+          offreEngagementQueryModel()
 
-      getServicesCiviqueQueryHandler.execute.resolves(
-        success(serviceCiviqueQueryModels)
-      )
+        getServicesCiviqueQueryHandler.execute.resolves(
+          success(serviceCiviqueQueryModels)
+        )
 
-      // When
-      await request(app.getHttpServer())
-        .get('/services-civique')
-        .set('authorization', unHeaderAuthorization())
-        .query(findServicesCiviqueQuery)
-        // Then
-        .expect(HttpStatus.OK)
+        // When
+        await request(app.getHttpServer())
+          .get('/services-civique')
+          .set('authorization', unHeaderAuthorization())
+          .query(findServicesCiviqueQuery)
+          // Then
+          .expect(HttpStatus.OK)
 
-      expect(getServicesCiviqueQueryHandler.execute).to.have.been.calledWith(
-        expectedQuery
-      )
+        expect(getServicesCiviqueQueryHandler.execute).to.have.been.calledWith(
+          expectedQuery
+        )
+      })
+    })
+    describe('quand il y a une failure http', () => {
+      it('retourne une erreur http', async () => {
+        // Given
+        const findServicesCiviqueQuery = {
+          page: '1',
+          limit: '50',
+          dateDeDebutMaximum: '2022-02-17T10:00:00Z',
+          dateDeDebutMinimum: '2022-02-17T10:00:00Z',
+          lat: '48.86899229710103',
+          lon: '2.3342718577284205',
+          distance: '10',
+          domaine: 'environnement'
+        }
+
+        getServicesCiviqueQueryHandler.execute.resolves(
+          failure(new ErreurHttp('Bad request', 400))
+        )
+
+        // When
+        await request(app.getHttpServer())
+          .get('/services-civique')
+          .set('authorization', unHeaderAuthorization())
+          .query(findServicesCiviqueQuery)
+          // Then
+          .expect(HttpStatus.BAD_REQUEST)
+      })
     })
     ensureUserAuthenticationFailsIfInvalid('get', '/services-civique')
   })
