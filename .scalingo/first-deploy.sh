1c95f4f1734df7480fa04bb7d88a541154791165
Joseph Robert <jrobert@octo.com>
2022-03-04 14:20:05 +0100

    plop

--- .scalingo/first-deploy.sh
+++ .scalingo/first-deploy.sh
@@ -8,7 +8,7 @@ if [[ $APP =~ "pa-back-staging-pr" ]] ; then
   psql --dbname $DATABASE_URL -c "CREATE extension postgis;"
   psql --dbname $DATABASE_URL -c "CREATE extension postgis_tiger_geocoder CASCADE;"
   psql --dbname $DATABASE_URL -c "CREATE extension postgis_topology;"
-  pg_dump --format c --data-only --no-owner --no-privileges --no-comments --schema 'sequelize' --schema 'public' --dbname $STAGING_DATABASE_URL --file dump.pgsql
+  pg_dump --clean --if-exists --format c --no-owner --no-privileges --no-comments --schema 'sequelize' --schema 'public' --exclude-schema 'topology' --dbname $STAGING_DATABASE_URL --file dump.pgsql
   pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname $DATABASE_URL dump.pgsql
 fi
 
--- .scalingo/post-deploy.sh
+++ .scalingo/post-deploy.sh
@@ -5,7 +5,7 @@ echo "Post deploy"
 if [[ $APP =~ "pa-back-staging-pr" ]] ; then
   echo "Dump de la DB de staging"
   dbclient-fetcher psql
-  pg_dump --format c --data-only --no-owner --no-privileges --no-comments --schema 'sequelize' --schema 'public' --dbname $STAGING_DATABASE_URL --file dump.pgsql
+  pg_dump --clean --if-exists --format c --no-owner --no-privileges --no-comments --schema 'sequelize' --schema 'public' --exclude-schema 'topology' --dbname $STAGING_DATABASE_URL --file dump.pgsql
   pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname $DATABASE_URL dump.pgsql
 fi
 
