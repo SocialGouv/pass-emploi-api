24df5391efe390ed41ea150e238ac27689c1a4c7
Joseph Robert <jrobert@octo.com>
2022-03-04 10:58:29 +0100

    fix: ne pas dump le schema topology

---
+++ .scalingo/first-deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "First deploy"
+
+if [[ $APP =~ "pa-back-staging-pr" ]] ; then
+  echo "Dump de la DB de staging"
+  dbclient-fetcher psql
+  psql --dbname $DATABASE_URL -c "CREATE extension postgis;"
+  psql --dbname $DATABASE_URL -c "CREATE extension postgis_tiger_geocoder CASCADE;"
+  psql --dbname $DATABASE_URL -c "CREATE extension postgis_topology;"
+  pg_dump --clean --if-exists --format c --no-owner --no-privileges --no-comments --exclude-schema 'information_schema' --exclude-schema '^pg_*' --exclude-schema '^tiger*' --exclude-schema 'topology' --dbname $STAGING_DATABASE_URL --file dump.pgsql
+  pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname $DATABASE_URL dump.pgsql
+fi
+
+yarn migration
