a43bcfafa6d97189a58bc3ba80cef01a610ca48c
Joseph Robert <jrobert@octo.com>
2022-03-04 10:58:29 +0100

    fix: ne pas dump le schema topology

--- .scalingo/post-deploy.sh
+++ .scalingo/post-deploy.sh
@@ -3,7 +3,7 @@
 if [[ $APP =~ "pa-back-staging-pr" ]] ; then
   echo "Dump de la DB de staging"
   dbclient-fetcher psql
-  pg_dump --clean --if-exists --format c --no-owner --no-privileges --no-comments --exclude-schema 'information_schema' --exclude-schema '^pg_*' --dbname $STAGING_DATABASE_URL --file dump.pgsql
+  pg_dump --format c --data-only --no-owner --no-privileges --no-comments --schema 'sequelize' --schema 'public' --dbname $STAGING_DATABASE_URL --file dump.pgsql
   pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname $DATABASE_URL dump.pgsql
 fi
 
