219dc358832e83e09ac47b20d090a23ed9094e74
Malek Zemni <3781318-Mzem@users.noreply.gitlab.com>
2022-03-28 16:13:59 +0200

    feat: tri actions pe

--- src/application/queries/get-actions-jeune-pole-emploi.query.handler.ts
+++ src/application/queries/get-actions-jeune-pole-emploi.query.handler.ts
@@ -4,6 +4,7 @@ import {
   NonTrouveError
 } from 'src/building-blocks/types/domain-error'
 import { failure, Result, success } from 'src/building-blocks/types/result'
+import { ActionPoleEmploi } from 'src/domain/action'
 import { Authentification } from 'src/domain/authentification'
 import { Jeune, JeunesRepositoryToken } from 'src/domain/jeune'
 import { DateService } from 'src/utils/date-service'
@@ -17,6 +18,8 @@ import {
 import { JeunePoleEmploiAuthorizer } from '../authorizers/authorize-jeune-pole-emploi'
 import { fromDemarcheDtoToActionPoleEmploiQueryModel } from './query-mappers/actions-pole-emploi.mappers'
 import { ActionPoleEmploiQueryModel } from './query-models/actions.query-model'
+
+const Statut = ActionPoleEmploi.Statut
 
 export interface GetActionsJeunePoleEmploiQuery extends Query {
   idJeune: string
@@ -53,15 +56,17 @@ export class GetActionsJeunePoleEmploiQueryHandler extends QueryHandler<
 
       const demarchesDto: DemarcheDto[] = responseDemarches?.data ?? []
 
-      return success(
-        demarchesDto.map(demarcheDto =>
+      const demarches = demarchesDto
+        .map(demarcheDto =>
           fromDemarcheDtoToActionPoleEmploiQueryModel(
             demarcheDto,
             this.idService,
             this.dateService
           )
         )
-      )
+        .sort(sortDemarchesByStatut)
+
+      return success(demarches)
     } catch (e) {
       this.logger.error(e)
       if (e.response) {
@@ -83,4 +88,46 @@ export class GetActionsJeunePoleEmploiQueryHandler extends QueryHandler<
   async monitor(): Promise<void> {
     return
   }
+}
+
+function sortDemarchesByStatut(
+  demarche1: ActionPoleEmploiQueryModel,
+  demarche2: ActionPoleEmploiQueryModel
+): number {
+  if (statutEgal(demarche1.statut, demarche2.statut)) {
+    const date1 = new Date(demarche1.dateFin).getTime()
+    const date2 = new Date(demarche2.dateFin).getTime()
+
+    if (date1 < date2) return -1
+    if (date1 > date2) return 1
+    return 0
+  }
+  if (stautInferieur(demarche1.statut, demarche2.statut)) {
+    return -1
+  }
+  return 1
+}
+
+const statutsEnCours = [Statut.EN_COURS, Statut.A_FAIRE]
+const statutsTermines = [Statut.REALISEE, Statut.ANNULEE]
+
+function statutEgal(
+  statut1: ActionPoleEmploi.Statut,
+  statut2: ActionPoleEmploi.Statut
+): boolean {
+  return (
+    statut1 === statut2 ||
+    (statutsEnCours.includes(statut1) && statutsEnCours.includes(statut2)) ||
+    (statutsTermines.includes(statut1) && statutsTermines.includes(statut2))
+  )
+}
+
+function stautInferieur(
+  statut1: ActionPoleEmploi.Statut,
+  statut2: ActionPoleEmploi.Statut
+): boolean {
+  if (statut1 === Statut.EN_RETARD) return true
+  if (statutsEnCours.includes(statut1) && statutsTermines.includes(statut2))
+    return true
+  return false
 }
