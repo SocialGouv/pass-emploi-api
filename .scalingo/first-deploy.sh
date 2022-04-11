b9a59d644b7907c9a18e7f3a465a2ca7d3e007f0
Malek Zemni <3781318-Mzem@users.noreply.gitlab.com>
2022-04-11 11:27:47 +0200

    fix: ajouter l'extension pg_trgm en first-deploy

--- .scalingo/first-deploy.sh
+++ .scalingo/first-deploy.sh
@@ -8,6 +8,7 @@ if [[ $APP =~ "pa-back-staging-pr" ]] ; then
   psql --dbname $DATABASE_URL -c "CREATE extension postgis;"
   psql --dbname $DATABASE_URL -c "CREATE extension postgis_tiger_geocoder CASCADE;"
   psql --dbname $DATABASE_URL -c "CREATE extension postgis_topology;"
+  psql --dbname $DATABASE_URL -c "CREATE extension pg_trgm;"
   pg_dump --clean --if-exists --format c --no-owner --no-privileges --no-comments -n 'public' -n 'sequelize' --exclude-schema 'information_schema' --exclude-schema '^pg_*' --exclude-schema 'tiger' --exclude-schema 'tiger_data' --exclude-schema 'topology' --dbname $STAGING_DATABASE_URL --file dump.pgsql
   pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname $DATABASE_URL dump.pgsql
 fi
