8b331ae1aaef04f98cfa0ec6957b68999afeb0dd
Joseph Robert <joseph.robert@octo.com>
2022-04-14 17:34:16 +0200

    refacto: dead code is dead

--- src/infrastructure/routes/validation/utils/transformers.ts
+++ src/infrastructure/routes/validation/utils/transformers.ts
@@ -27,14 +27,6 @@ export function transformStringToBoolean(
   return params.obj[key]
 }
 
-export function transformStringToDate(
-  params: TransformFnParams,
-  key: string
-): Date {
-  params.obj[key] = new Date(params.value)
-  return params.obj[key]
-}
-
 export function transformStringToInteger(
   params: TransformFnParams,
   key: string
