112d2cf9a91e5afff1e8ddf15d3f8e779c4c8fed
Joseph Robert <joseph.robert@octo.com>
2022-04-20 16:25:50 +0200

    feat: ajouter un identifiant unique aux agences

--- src/infrastructure/sequelize/migrations/20220420132138-ajouter-table-agences.js
+++ src/infrastructure/sequelize/migrations/20220420132138-ajouter-table-agences.js
@@ -4,9 +4,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('agence', {
       id: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
         primaryKey: true,
-        autoIncrement: true,
         allowNull: false
       },
       nomAgence: {
