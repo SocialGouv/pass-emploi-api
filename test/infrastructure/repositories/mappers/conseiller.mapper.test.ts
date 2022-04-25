f07e55145afc1e455941b9f210fcfa22273fd667
remi <remi.dormoy@octo.com>
2022-04-25 11:46:58 +0200

    feat: ajout de tests sur le mapper

---
+++ test/infrastructure/repositories/mappers/conseiller.mapper.test.ts
@@ -0,0 +1,94 @@
+import { fromSqlToDetailConseillerQueryModel } from '../../../../src/infrastructure/repositories/mappers/conseillers.mappers'
+import { ConseillerSqlModel } from '../../../../src/infrastructure/sequelize/models/conseiller.sql-model'
+import { DatabaseForTesting, expect } from '../../../utils'
+import { unConseillerDto } from '../../../fixtures/sql-models/conseiller.sql-model'
+import {
+  AgenceDto,
+  AgenceSqlModel
+} from '../../../../src/infrastructure/sequelize/models/agence.sql-model'
+
+describe('fromSqlToDetailConseillerQueryModel', () => {
+  before(() => {
+    DatabaseForTesting.prepare()
+  })
+
+  describe('sans agence', () => {
+    it('renvoie le query model', async () => {
+      // Given
+      const sql = await ConseillerSqlModel.create(unConseillerDto())
+
+      // When
+      const result = fromSqlToDetailConseillerQueryModel(sql)
+
+      // Then
+      expect(result).to.deep.equal({
+        id: '1',
+        firstName: 'Nils',
+        lastName: 'Tavernier',
+        agence: undefined
+      })
+    })
+  })
+
+  describe('avec agence non présente dans le référentiel', () => {
+    it('renvoie le query model', async () => {
+      // Given
+      const sql = await ConseillerSqlModel.create(
+        unConseillerDto({
+          nomManuelAgence: "nom d'agence"
+        })
+      )
+
+      // When
+      const result = fromSqlToDetailConseillerQueryModel(sql)
+
+      // Then
+      expect(result).to.deep.equal({
+        id: '1',
+        firstName: 'Nils',
+        lastName: 'Tavernier',
+        agence: {
+          id: undefined,
+          nom: "nom d'agence"
+        }
+      })
+    })
+  })
+
+  describe('avec agence présente dans le référentiel', () => {
+    it('renvoie le query model', async () => {
+      // Given
+      const uneAgence: Partial<AgenceDto> = {
+        id: "id d'agence",
+        nomAgence: 'Bonjour je suis une agence',
+        structure: 'MILO',
+        codeDepartement: '45',
+        nomRegion: 'yolo'
+      }
+      await AgenceSqlModel.create(uneAgence)
+      await ConseillerSqlModel.create(
+        unConseillerDto({
+          idAgence: "id d'agence"
+        })
+      )
+
+      const sql = await ConseillerSqlModel.findByPk('1', {
+        include: [AgenceSqlModel]
+      })
+
+      // When
+      const result = fromSqlToDetailConseillerQueryModel(sql!)
+
+      // Then
+      expect(result).to.deep.equal({
+        id: '1',
+        firstName: 'Nils',
+        lastName: 'Tavernier',
+        agence: {
+          id: "id d'agence",
+          nom: 'Bonjour je suis une agence'
+        }
+      })
+    })
+  })
+})
