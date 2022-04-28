22c25130d793d3d52385f451b486ec389e6e7c22
remi <remi.dormoy@octo.com>
2022-04-28 10:57:03 +0200

    code review

--- src/application/queries/query-models/conseillers.query-models.ts
+++ src/application/queries/query-models/conseillers.query-models.ts
@@ -1,25 +1,19 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 import { AgenceQueryModel } from './agence.query-models'
-import { IsString } from 'class-validator'
 
 export class DetailConseillerQueryModel {
   @ApiProperty()
-  @IsString()
   id: string
 
   @ApiProperty()
-  @IsString()
   firstName: string
 
   @ApiProperty()
-  @IsString()
   lastName: string
 
   @ApiPropertyOptional()
-  @IsString()
   email?: string
 
   @ApiProperty({ required: false })
-  @IsString()
   agence?: AgenceQueryModel
 }
