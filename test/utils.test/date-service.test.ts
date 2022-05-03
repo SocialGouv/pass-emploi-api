c9770304b0619af9358151332b485aa801f08df1
Joseph Robert <joseph.robert@octo.com>
2022-05-03 08:18:25 +0200

    refactor: un beau describe dans les tests du date service

--- test/utils.test/date-service.test.ts
+++ test/utils.test/date-service.test.ts
@@ -4,21 +4,23 @@ import { DateService } from 'src/utils/date-service'
 
 const dateService = new DateService()
 
-describe('isSameDateDay', () => {
-  it('retourne true si la date est au meme jour, meme mois et meme année', () => {
-    const date1 = DateTime.fromISO('2020-04-06T12:00:00.000Z').toUTC()
-    const date2 = DateTime.fromISO('2020-04-06T17:03:12.000Z').toUTC()
+describe('DateService', () => {
+  describe('isSameDateDay', () => {
+    it('retourne true si la date est au meme jour, meme mois et meme année', () => {
+      const date1 = DateTime.fromISO('2020-04-06T12:00:00.000Z').toUTC()
+      const date2 = DateTime.fromISO('2020-04-06T17:03:12.000Z').toUTC()
 
-    expect(dateService.isSameDateDay(date1, date2)).to.equal(true)
+      expect(dateService.isSameDateDay(date1, date2)).to.equal(true)
+    })
   })
-})
 
-describe('fromISOStringToUTCJSDate', () => {
-  it('retourne une date JS en UTC', () => {
-    const dateString = '2020-04-06T12:00:00+02:00'
-    const dateUTC = dateService.fromISOStringToUTCJSDate(dateString)
-    const expectedDateUTC = new Date('2020-04-06T10:00:00.000Z')
+  describe('fromISOStringToUTCJSDate', () => {
+    it('retourne une date JS en UTC', () => {
+      const dateString = '2020-04-06T12:00:00+02:00'
+      const dateUTC = dateService.fromISOStringToUTCJSDate(dateString)
+      const expectedDateUTC = new Date('2020-04-06T10:00:00.000Z')
 
-    expect(dateUTC).to.deep.equal(expectedDateUTC)
+      expect(dateUTC).to.deep.equal(expectedDateUTC)
+    })
   })
 })
