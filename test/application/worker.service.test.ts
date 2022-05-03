be08d09933a518430dbb3f196423cd2f4b2523c5
Joseph Robert <joseph.robert@octo.com>
2022-05-03 08:12:07 +0200

    test: worker service en intégration avec redis

---
+++ test/application/worker.service.test.ts
@@ -0,0 +1,76 @@
+import { WorkerService } from '../../src/application/worker.service'
+import { DatabaseForTesting, expect, StubbedClass, stubClass } from '../utils'
+import { HandleJobMailConseillerCommandHandler } from '../../src/application/commands/jobs/handle-job-mail-conseiller.command'
+import { HandleJobRendezVousCommandHandler } from '../../src/application/commands/jobs/handle-job-rendez-vous.command'
+import { HandleNettoyerLesJobsCommandHandler } from '../../src/application/commands/jobs/handle-job-nettoyer-les-jobs.command'
+import { HandleJobUpdateMailingListConseillerCommandHandler } from '../../src/application/commands/jobs/handle-job-update-mailing-list-conseiller.command'
+import { HandleJobNotifierNouveauxServicesCiviqueCommandHandler } from '../../src/application/commands/jobs/handle-job-notification-recherche-service-civique.command.handler'
+import { HandleJobNotifierNouvellesOffresEmploiCommandHandler } from '../../src/application/commands/jobs/handle-job-notifier-nouvelles-offres-emploi.command'
+import { PlanificateurRedisRepository } from '../../src/infrastructure/repositories/planificateur-redis.repository'
+import { testConfig } from '../utils/module-for-testing'
+import { DateService } from '../../src/utils/date-service'
+import { Planificateur } from '../../src/domain/planificateur'
+
+describe('WorkerService', () => {
+  describe('handler', () => {
+    let handleNettoyerLesJobsCommandHandler: StubbedClass<HandleNettoyerLesJobsCommandHandler>
+
+    beforeEach(async () => {
+      // Given
+      DatabaseForTesting.prepare()
+      const planificateurRepository = new PlanificateurRedisRepository(
+        testConfig(),
+        new DateService()
+      )
+      await planificateurRepository.isQueueReady()
+
+      const handlerJobRendezVousCommandHandler = stubClass(
+        HandleJobRendezVousCommandHandler
+      )
+      const handleJobMailConseillerCommandHandler = stubClass(
+        HandleJobMailConseillerCommandHandler
+      )
+      const notifierNouvellesOffresEmploiCommandHandler = stubClass(
+        HandleJobNotifierNouvellesOffresEmploiCommandHandler
+      )
+      handleNettoyerLesJobsCommandHandler = stubClass(
+        HandleNettoyerLesJobsCommandHandler
+      )
+      const handleJobUpdateMailingListConseillerCommandHandler = stubClass(
+        HandleJobUpdateMailingListConseillerCommandHandler
+      )
+      const handleJobNotifierNouveauxServicesCiviqueCommandHandler = stubClass(
+        HandleJobNotifierNouveauxServicesCiviqueCommandHandler
+      )
+      const workerService = new WorkerService(
+        planificateurRepository,
+        handlerJobRendezVousCommandHandler,
+        handleJobMailConseillerCommandHandler,
+        notifierNouvellesOffresEmploiCommandHandler,
+        handleNettoyerLesJobsCommandHandler,
+        handleJobUpdateMailingListConseillerCommandHandler,
+        handleJobNotifierNouveauxServicesCiviqueCommandHandler
+      )
+
+      workerService.subscribe()
+
+      // When
+      const job: Planificateur.Job = {
+        date: new Date(),
+        type: Planificateur.CronJob.NETTOYER_LES_JOBS,
+        contenu: { message: 'my test dummy job' }
+      }
+      await planificateurRepository.createJob(job)
+    })
+
+    it('exécute la commande idoine', done => {
+      // Then
+      setTimeout(() => {
+        expect(
+          handleNettoyerLesJobsCommandHandler.execute
+        ).to.have.been.calledWith()
+        done()
+      }, 2000)
+    })
+  })
+})
