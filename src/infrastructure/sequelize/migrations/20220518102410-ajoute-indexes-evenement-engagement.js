9cc755c7beb4be975f45136426e730415b32b0f8
Adrien Graux <adrien.graux@octo.com>
2022-05-18 13:11:45 +0200

    fix: ajoute des indexes sur la table evenement_engagement

---
+++ src/infrastructure/sequelize/migrations/20220518102410-ajoute-indexes-evenement-engagement.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.transaction(async transaction => {
+      await queryInterface.addIndex('evenement_engagement', ['id_utilisateur'], {
+        transaction,
+        name: 'idx_evenement_engagement_id_utilisateur'
+      })
+
+      await queryInterface.addIndex('evenement_engagement', ['type_utilisateur'], {
+        transaction,
+        name: 'idx_evenement_engagement_type_utilisateur'
+      })
+
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.transaction(async transaction => {
+      await queryInterface.removeIndex('evenement_engagement', 'idx_evenement_engagement_id_utilisateur', {transaction})
+      await queryInterface.removeIndex('evenement_engagement', 'idx_evenement_engagement_type_utilisateur', {transaction})
+    })
+  }
+};
