f5917e43ee077524ca866501f86612f2ccaffc13
Adrien Graux <adrien.graux@octo.com>
2022-05-25 12:20:09 +0200

    chore: extract swagger as middleware

--- src/main.ts
+++ src/main.ts
@@ -5,56 +5,12 @@ import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
-import {
-  DocumentBuilder,
-  SwaggerCustomOptions,
-  SwaggerModule
-} from '@nestjs/swagger'
-import { SecuritySchemeObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface'
+import helmet from 'helmet'
 import { Logger } from 'nestjs-pino'
 import { AppModule } from './app.module'
 import { TaskService } from './application/task.service'
 import { WorkerService } from './application/worker.service'
-import helmet from 'helmet'
-
-function useSwagger(
-  appConfig: ConfigService,
-  app: NestExpressApplication
-): void {
-  const issuerUrl = appConfig.get('oidc.issuerUrl')
-  const baserUrl = appConfig.get('baseUrl')
-  const oauth2SecuritySchemeObject: SecuritySchemeObject = {
-    type: 'oauth2',
-    description: 'client_id: pass-emploi-swagger',
-    flows: {
-      implicit: {
-        authorizationUrl: `${issuerUrl}/protocol/openid-connect/auth`,
-        refreshUrl: `${issuerUrl}/protocol/openid-connect/token`,
-        tokenUrl: `${issuerUrl}/protocol/openid-connect/token`,
-        scopes: {}
-      }
-    }
-  }
-  const apiKeySecuritySchemeObject: SecuritySchemeObject = {
-    type: 'apiKey',
-    in: 'header',
-    name: 'X-API-KEY'
-  }
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('Pass Emploi Api')
-    .setVersion('1.0')
-    .addOAuth2(oauth2SecuritySchemeObject)
-    .addSecurity('api_key', apiKeySecuritySchemeObject)
-    .build()
-  const document = SwaggerModule.createDocument(app, swaggerConfig)
-  const customOptions: SwaggerCustomOptions = {
-    swaggerOptions: {
-      persistAuthorization: true,
-      oauth2RedirectUrl: `${baserUrl}/documentation/oauth2-redirect.html`
-    }
-  }
-  SwaggerModule.setup('documentation', app, document, customOptions)
-}
+import { useSwagger } from './infrastructure/middlewares/swagger.middleware'
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
