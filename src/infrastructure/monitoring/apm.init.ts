e21d13279b8f904788cdb6183c53db6b6cf5c50c
Joseph Robert <joseph.robert@octo.com>
2022-07-06 15:06:34 +0200

    fix: activer l'apm uniquement sur le web

--- src/infrastructure/monitoring/apm.init.ts
+++ src/infrastructure/monitoring/apm.init.ts
@@ -4,10 +4,10 @@ import * as APM from 'elastic-apm-node'
 let instance: APM.Agent | undefined
 
 const serviceName = process.env.APM_SERVICE_NAME || 'pa-back-local'
-const secretToken = process.env.APM_SECRET_TOKEN  || ''
+const secretToken = process.env.APM_SECRET_TOKEN || ''
 const serverUrl = process.env.APM_URL || ''
 const environment = process.env.ENVIRONMENT || 'development'
-const active =  process.env.APM_IS_ACTIVE === 'true'
+const active = process.env.APM_IS_ACTIVE === 'true' && process.env.IS_WEB === 'true'
 
 const config: APM.AgentConfigOptions = {
   serviceName,
