e930242465264f92440c579d4a6157ef004b324a
Joseph Robert <joseph.robert@octo.com>
2022-08-26 13:45:00 +0200

    feat: ne pas sauvegarder la qualification si Milo ne répond pas

--- src/domain/action/milo.ts
+++ src/domain/action/milo.ts
@@ -1,7 +1,12 @@
 import { Action } from './action'
+import { Authentification } from '../authentification'
+import { Result } from '../../building-blocks/types/result'
 
 export namespace Milo {
   export interface Repository {
-    save(action: Action): Promise<void>
+    save(
+      action: Action,
+      utilisateur: Authentification.Utilisateur
+    ): Promise<Result>
   }
 }
