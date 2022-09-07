6e1d4b7d32540b6b7e2fa86ac5852c6c4e13c21f
Malek Zemni <malek.zemni@octo.com>
2022-09-07 12:07:51 +0200

    fix: review app dump error

--- .scalingo/post-deploy.sh
+++ .scalingo/post-deploy.sh
@@ -3,8 +3,8 @@
 if [[ $APP =~ "pa-back-staging-pr" ]] ; then
   echo "Dump de la DB de staging"
   dbclient-fetcher psql
-  pg_dump --clean --if-exists --format c --no-owner --no-privileges --no-comments -n 'public' -n 'sequelize' --exclude-schema 'information_schema' --exclude-schema '^pg_*' --exclude-schema 'tiger' --exclude-schema 'tiger_data' --exclude-schema 'topology' --dbname $STAGING_DATABASE_URL --file dump.pgsql
-  pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname $DATABASE_URL dump.pgsql
+  pg_dump --clean --if-exists --format c --no-owner --no-privileges --no-comments -n 'public' -n 'sequelize' --exclude-schema 'information_schema' --exclude-schema '^pg_*' --exclude-schema 'tiger' --exclude-schema 'tiger_data' --exclude-schema 'topology' --exclude-schema 'spatial_ref_sys' --dbname $STAGING_DATABASE_URL --file dump.pgsql
+  pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --exclude-schema 'public' --dbname $DATABASE_URL dump.pgsql
 fi
 
 yarn migration
