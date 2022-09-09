d17165ce9701b730dda2cd1f2d49fc751ea7d514
Joseph Robert <joseph.robert@octo.com>
2022-09-09 09:50:55 +0200

    feat(986): job pour maj les codes événements

--- src/infrastructure/repositories/evenement-http-sql.repository.db.ts
+++ src/infrastructure/repositories/evenement-http-sql.repository.db.ts
@@ -1,23 +1,19 @@
 import { Injectable } from '@nestjs/common'
-import { Authentification } from '../../domain/authentification'
 import { Evenement } from '../../domain/evenement'
 import { emptySuccess, Result } from '../../building-blocks/types/result'
 import { EvenementEngagementSqlModel } from '../sequelize/models/evenement-engagement.sql-model'
 
 @Injectable()
-export class EvenementHttpSqlRepository implements Evenement.Repository {
-  async save(
-    utilisateur: Authentification.Utilisateur,
-    evenement: Evenement
-  ): Promise<Result> {
+export class EvenementSqlRepository implements Evenement.Repository {
+  async save(evenement: Evenement): Promise<Result> {
     await EvenementEngagementSqlModel.create({
       code: evenement.code ?? null,
       categorie: evenement.categorie ?? null,
       action: evenement.action ?? null,
       nom: evenement.nom ?? null,
-      idUtilisateur: utilisateur.id,
-      typeUtilisateur: utilisateur.type,
-      structure: utilisateur.structure,
+      idUtilisateur: evenement.utilisateur.id,
+      typeUtilisateur: evenement.utilisateur.type,
+      structure: evenement.utilisateur.structure,
       dateEvenement: evenement.date
     })
     return emptySuccess()
