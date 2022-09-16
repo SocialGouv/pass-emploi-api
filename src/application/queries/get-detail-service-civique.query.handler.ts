a72c1909e6da7b64b7d42467b8e9d6e0871a85f9
Amelle El Bakkal <amelle.el.bakkal@octo.com>
2022-09-16 11:12:40 +0200

    fix: Retours revue corrige authorizer et promise all

--- src/application/queries/get-detail-service-civique.query.handler.ts
+++ src/application/queries/get-detail-service-civique.query.handler.ts
@@ -70,7 +70,7 @@ export class GetDetailServiceCiviqueQueryHandler extends QueryHandler<
 
   async monitor(utilisateur: Authentification.Utilisateur): Promise<void> {
     await this.evenementService.creer(
-      Evenement.Code.OFFRE_SERVICE_CIVIQUE_AFFICHEE,
+      Evenement.Code.OFFRE_SERVICE_CIVIQUE_AFFICHE,
       utilisateur
     )
   }
