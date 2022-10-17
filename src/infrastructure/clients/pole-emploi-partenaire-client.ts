825fcd5c8c89f5ea7bab6bd7494b98b8c892471f
Joseph Robert <joseph.robert@octo.com>
2022-10-17 14:55:28 +0200

    plop

--- src/infrastructure/clients/pole-emploi-partenaire-client.ts
+++ src/infrastructure/clients/pole-emploi-partenaire-client.ts
@@ -134,20 +134,14 @@ export class PoleEmploiPartenaireClient implements PoleEmploiPartenaireClientI {
     try {
       const body = {
         id: demarcheModifiee.id,
-        dateModification: demarcheModifiee.dateModification.toUTC().toISO({
-          includeOffset: false
-        }),
+        dateModification: demarcheModifiee.dateModification.toUTC().toISO(),
         origineModification: ORIGINE,
         etat: toEtat(demarcheModifiee.statut),
         dateDebut: demarcheModifiee.dateDebut
-          ? demarcheModifiee.dateDebut?.toUTC().toISO({ includeOffset: false })
+          ? demarcheModifiee.dateDebut?.toUTC().toISO()
           : undefined,
-        dateFin: demarcheModifiee.dateFin
-          ?.toUTC()
-          .toISO({ includeOffset: false }),
-        dateAnnulation: demarcheModifiee.dateAnnulation?.toUTC().toISO({
-          includeOffset: false
-        })
+        dateFin: demarcheModifiee.dateFin?.toUTC().toISO(),
+        dateAnnulation: demarcheModifiee.dateAnnulation?.toUTC().toISO()
       }
       this.logger.log(body)
 
@@ -175,10 +169,8 @@ export class PoleEmploiPartenaireClient implements PoleEmploiPartenaireClientI {
       const body = {
         origineCreateur: ORIGINE,
         etat: toEtat(demarche.statut),
-        dateCreation: demarche.dateCreation.toUTC().toISO({
-          includeOffset: false
-        }),
-        dateFin: demarche.dateFin.toUTC().toISO({ includeOffset: false }),
+        dateCreation: demarche.dateCreation.toUTC().toISO(),
+        dateFin: demarche.dateFin.toUTC().toISO(),
         pourquoi: demarche.pourquoi,
         quoi: demarche.quoi,
         comment: demarche.comment,
