5fb802a683365f958645fe5ef8d88ac3b206ef51
Sonny Klotz <klotz.sonny@gmail.com>
2022-11-02 16:53:09 +0100

    refactor: conseiller factory mettre a jour retourne result

--- src/application/commands/modifier-conseiller.command.handler.ts
+++ src/application/commands/modifier-conseiller.command.handler.ts
@@ -2,20 +2,18 @@ import { Inject, Injectable } from '@nestjs/common'
 import { CommandHandler } from '../../building-blocks/types/command-handler'
 import {
   DroitsInsuffisants,
-  MauvaiseCommandeError,
   NonTrouveError
 } from '../../building-blocks/types/domain-error'
 import { Query } from '../../building-blocks/types/query'
 import {
   emptySuccess,
   failure,
+  isFailure,
   Result
 } from '../../building-blocks/types/result'
 import { Agence, AgenceRepositoryToken } from '../../domain/agence'
 import { Authentification } from '../../domain/authentification'
 import { Conseiller, ConseillersRepositoryToken } from '../../domain/conseiller'
-import { Core } from '../../domain/core'
-import Structure = Core.Structure
 
 export interface ModifierConseillerCommand extends Query {
   idConseiller: string
@@ -32,7 +30,8 @@ export class ModifierConseillerCommandHandler extends CommandHandler<
     @Inject(ConseillersRepositoryToken)
     private conseillerRepository: Conseiller.Repository,
     @Inject(AgenceRepositoryToken)
-    private agencesRepository: Agence.Repository
+    private agencesRepository: Agence.Repository,
+    private conseillerFactory: Conseiller.Factory
   ) {
     super('ModifierConseillerCommandHandler')
   }
@@ -41,52 +40,34 @@ export class ModifierConseillerCommandHandler extends CommandHandler<
     const conseillerActuel = await this.conseillerRepository.get(
       command.idConseiller
     )
-    if (conseillerActuel) {
-      if (command.agence) {
-        if (conseillerActuel.structure === Structure.POLE_EMPLOI) {
-          if (command.agence?.id) {
-            const agence = await this.agencesRepository.get(
-              command.agence.id,
-              conseillerActuel.structure
-            )
-            if (!agence) {
-              return failure(new NonTrouveError('Agence', command.agence.id))
-            }
-          }
-        } else if (conseillerActuel.structure === Structure.MILO) {
-          if (command.agence?.id) {
-            const agence = await this.agencesRepository.get(
-              command.agence.id,
-              conseillerActuel.structure
-            )
-            if (!agence) {
-              return failure(new NonTrouveError('Agence', command.agence.id))
-            }
-          } else {
-            return failure(
-              new MauvaiseCommandeError(
-                'L’agence renseignée doit provenir du référentiel.'
-              )
-            )
-          }
-        }
-      }
-
-      const infosDeMiseAJour: Conseiller.InfoDeMiseAJour = {
-        notificationsSonores:
-          command.notificationsSonores ?? conseillerActuel.notificationsSonores,
-        agence: command.agence ?? conseillerActuel.agence
-      }
-
-      const conseiller = Conseiller.mettreAJour(
-        conseillerActuel,
-        infosDeMiseAJour
-      )
-      await this.conseillerRepository.save(conseiller)
-      return emptySuccess()
-    } else {
+    if (!conseillerActuel) {
       return failure(new NonTrouveError('Conseiller', command.idConseiller))
     }
+
+    if (command.agence?.id) {
+      const agence = await this.agencesRepository.get(
+        command.agence.id,
+        conseillerActuel.structure
+      )
+      if (!agence) {
+        return failure(new NonTrouveError('Agence', command.agence.id))
+      }
+    }
+
+    const infosDeMiseAJour: Conseiller.InfosDeMiseAJour = {
+      notificationsSonores:
+        command.notificationsSonores ?? conseillerActuel.notificationsSonores,
+      agence: command.agence ?? conseillerActuel.agence
+    }
+
+    const conseillerResult: Result<Conseiller> =
+      this.conseillerFactory.mettreAJour(conseillerActuel, infosDeMiseAJour)
+
+    if (isFailure(conseillerResult)) {
+      return conseillerResult
+    }
+    await this.conseillerRepository.save(conseillerResult.data)
+    return emptySuccess()
   }
 
   async authorize(
