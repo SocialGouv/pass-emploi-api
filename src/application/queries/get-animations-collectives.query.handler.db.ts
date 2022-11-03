ae8cc143a1b8697ee3b2c5b9040dea29b852f108
Malek Zemni <malek.zemni@octo.com>
2022-11-03 11:43:08 +0100

    feat(1172): récupérer animations collectives

---
+++ src/application/queries/get-animations-collectives.query.handler.db.ts
@@ -0,0 +1,116 @@
+import { Injectable } from '@nestjs/common'
+import { DateTime } from 'luxon'
+import { Op } from 'sequelize'
+import { Query } from '../../building-blocks/types/query'
+import { QueryHandler } from '../../building-blocks/types/query-handler'
+import { Result, success } from '../../building-blocks/types/result'
+import { Authentification } from '../../domain/authentification'
+import { CodeTypeRendezVous, RendezVous } from '../../domain/rendez-vous'
+import { JeuneSqlModel } from '../../infrastructure/sequelize/models/jeune.sql-model'
+import { RendezVousSqlModel } from '../../infrastructure/sequelize/models/rendez-vous.sql-model'
+import { DateService } from '../../utils/date-service'
+import { ConseillerEtablissementAuthorizer } from '../authorizers/authorize-conseiller-etablissement'
+import { fromSqlToRendezVousConseillerQueryModel } from './query-mappers/rendez-vous-milo.mappers'
+import { AnimationCollectiveQueryModel } from './query-models/rendez-vous.query-model'
+
+const NOMBRE_ANIMATIONS_COLLECTIVES_MAX = 200
+
+export interface GetAnimationsCollectivesQuery extends Query {
+  idEtablissement: string
+  dateDebut?: DateTime
+  dateFin?: DateTime
+}
+
+@Injectable()
+export class GetAnimationsCollectivesQueryHandler extends QueryHandler<
+  GetAnimationsCollectivesQuery,
+  Result<AnimationCollectiveQueryModel[]>
+> {
+  constructor(
+    private conseillerAgenceAuthorizer: ConseillerEtablissementAuthorizer,
+    private dateService: DateService
+  ) {
+    super('GetAnimationsCollectivesQueryHandler')
+  }
+
+  async handle(
+    query: GetAnimationsCollectivesQuery
+  ): Promise<Result<AnimationCollectiveQueryModel[]>> {
+    const rdvSql = await RendezVousSqlModel.findAll({
+      where: {
+        idAgence: query.idEtablissement,
+        dateSuppression: {
+          [Op.is]: null
+        },
+        type: {
+          [Op.in]: [
+            CodeTypeRendezVous.INFORMATION_COLLECTIVE,
+            CodeTypeRendezVous.ATELIER
+          ]
+        },
+        ...generateDateCondition(query.dateDebut, query.dateFin)
+      },
+      include: [{ model: JeuneSqlModel }],
+      order: [['date', 'ASC']],
+      limit: NOMBRE_ANIMATIONS_COLLECTIVES_MAX
+    })
+
+    const maintenant = this.dateService.nowJs()
+
+    return success(
+      rdvSql.map(rdv => ({
+        ...fromSqlToRendezVousConseillerQueryModel(rdv),
+        statut: construireStatut(rdv, maintenant)
+      }))
+    )
+  }
+
+  async authorize(
+    query: GetAnimationsCollectivesQuery,
+    utilisateur: Authentification.Utilisateur
+  ): Promise<Result> {
+    return this.conseillerAgenceAuthorizer.authorize(
+      query.idEtablissement,
+      utilisateur
+    )
+  }
+
+  async monitor(): Promise<void> {
+    return
+  }
+}
+
+function generateDateCondition(
+  dateDebut?: DateTime,
+  dateFin?: DateTime
+): { date?: Record<string, string> } {
+  let dateCondition = undefined
+
+  if (dateDebut) {
+    dateCondition = {
+      [Op.gte]: dateDebut.toJSDate()
+    }
+  }
+  if (dateFin) {
+    dateCondition = {
+      ...dateCondition,
+      [Op.lte]: dateFin.toJSDate()
+    }
+  }
+
+  return dateCondition ? { date: dateCondition } : {}
+}
+
+function construireStatut(
+  rendezVousSql: RendezVousSqlModel,
+  maintenant: Date
+): RendezVous.AnimationCollective.Statut {
+  if (rendezVousSql.dateCloture === null) {
+    if (rendezVousSql.date <= maintenant) {
+      return RendezVous.AnimationCollective.Statut.A_CLOTURER
+    }
+    return RendezVous.AnimationCollective.Statut.A_VENIR
+  } else {
+    return RendezVous.AnimationCollective.Statut.CLOTUREE
+  }
+}
---
+++ src/infrastructure/routes/etablissements.controller.ts
@@ -0,0 +1,55 @@
+import { Controller, Get, Param, Query } from '@nestjs/common'
+import { ApiOAuth2, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { GetAnimationsCollectivesQueryHandler } from 'src/application/queries/get-animations-collectives.query.handler.db'
+import { AnimationCollectiveQueryModel } from 'src/application/queries/query-models/rendez-vous.query-model'
+import { isSuccess } from '../../building-blocks/types/result'
+import { Authentification } from '../../domain/authentification'
+import { Utilisateur } from '../decorators/authenticated.decorator'
+import { handleFailure } from './failure.handler'
+import { DateService } from '../../utils/date-service'
+import { GetAnimationsCollectivesQueryParams } from './validation/etablissements.inputs'
+
+@Controller('etablissements')
+@ApiOAuth2([])
+@ApiTags('Etablissements')
+export class EtablissementsController {
+  constructor(
+    private readonly getAnimationsCollectivesQueryHandler: GetAnimationsCollectivesQueryHandler
+  ) {}
+
+  @ApiOperation({
+    summary: "Récupère les animations collectives d'un établissement",
+    description:
+      "Autorisé pour un conseiller appartenant à l'établissement et ses jeuness"
+  })
+  @Get(':idEtablissement/animations-collectives')
+  @ApiResponse({
+    type: AnimationCollectiveQueryModel,
+    isArray: true
+  })
+  async getAnimationsCollectives(
+    @Param('idEtablissement') idEtablissement: string,
+    @Utilisateur() utilisateur: Authentification.Utilisateur,
+    @Query()
+    getAnimationsCollectivesQueryParams: GetAnimationsCollectivesQueryParams
+  ): Promise<AnimationCollectiveQueryModel[]> {
+    const result = await this.getAnimationsCollectivesQueryHandler.execute(
+      {
+        idEtablissement,
+        dateDebut: DateService.fromStringToLocaleDateTime(
+          getAnimationsCollectivesQueryParams.dateDebut
+        ),
+        dateFin: DateService.fromStringToLocaleDateTime(
+          getAnimationsCollectivesQueryParams.dateFin
+        )
+      },
+      utilisateur
+    )
+
+    if (isSuccess(result)) {
+      return result.data
+    }
+
+    throw handleFailure(result)
+  }
+}
