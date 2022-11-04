c4920c71e7d35bb21633e210213985b8f1952637
Amelle El Bakkal <amelle.el.bakkal@octo.com>
2022-11-04 12:00:59 +0100

    feat: récupère jeunes d’un établissement

---
+++ src/application/queries/get-jeunes-by-etablissement.query.handler.db.ts
@@ -0,0 +1,79 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { Sequelize } from 'sequelize'
+import { Authentification } from 'src/domain/authentification'
+import { Query } from '../../building-blocks/types/query'
+import { QueryHandler } from '../../building-blocks/types/query-handler'
+import { Result, success } from '../../building-blocks/types/result'
+import { Conseiller, ConseillersRepositoryToken } from '../../domain/conseiller'
+import { ConseillerSqlModel } from '../../infrastructure/sequelize/models/conseiller.sql-model'
+import { JeuneSqlModel } from '../../infrastructure/sequelize/models/jeune.sql-model'
+import { SequelizeInjectionToken } from '../../infrastructure/sequelize/providers'
+import { ConseillerEtablissementAuthorizer } from '../authorizers/authorize-conseiller-etablissement'
+import { JeuneQueryModel } from './query-models/jeunes.query-model'
+
+export interface GetJeunesByEtablissementQuery extends Query {
+  idEtablissement: string
+}
+
+@Injectable()
+export class GetJeunesByEtablissementQueryHandler extends QueryHandler<
+  GetJeunesByEtablissementQuery,
+  Result<JeuneQueryModel[]>
+> {
+  constructor(
+    @Inject(SequelizeInjectionToken) private readonly sequelize: Sequelize,
+    @Inject(ConseillerEtablissementAuthorizer)
+    private readonly conseillerEtablissementAuthorizer: ConseillerEtablissementAuthorizer,
+    @Inject(ConseillersRepositoryToken)
+    private readonly conseillersRepository: Conseiller.Repository
+  ) {
+    super('GetJeunesByEtablissementQueryHandler')
+  }
+
+  async handle(
+    query: GetJeunesByEtablissementQuery
+  ): Promise<Result<JeuneQueryModel[]>> {
+    const jeunes = await this.getAllQueryModelsByEtablissement(
+      query.idEtablissement
+    )
+    return success(jeunes)
+  }
+
+  async authorize(
+    { idEtablissement }: GetJeunesByEtablissementQuery,
+    utilisateur: Authentification.Utilisateur
+  ): Promise<Result> {
+    return this.conseillerEtablissementAuthorizer.authorize(
+      idEtablissement,
+      utilisateur
+    )
+  }
+
+  async monitor(): Promise<void> {
+    return
+  }
+
+  private async getAllQueryModelsByEtablissement(
+    idEtablissement: string
+  ): Promise<JeuneQueryModel[]> {
+    const sqlJeunes = await JeuneSqlModel.findAll({
+      include: [
+        {
+          model: ConseillerSqlModel,
+          required: true,
+          where: { idAgence: idEtablissement }
+        }
+      ],
+      order: [
+        ['prenom', 'ASC'],
+        ['nom', 'ASC']
+      ]
+    })
+
+    return sqlJeunes.map(jeune => ({
+      id: jeune.id,
+      firstName: jeune.prenom,
+      lastName: jeune.nom
+    }))
+  }
+}
