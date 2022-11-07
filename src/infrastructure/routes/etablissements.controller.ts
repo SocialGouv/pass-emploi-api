46adfb51a5448d46394fdff255004e4f784406ef
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2022-11-07 16:52:29 +0100

    feat: ajoute route recuperation jeunes d’un établissement

--- src/infrastructure/routes/etablissements.controller.ts
+++ src/infrastructure/routes/etablissements.controller.ts
@@ -2,11 +2,13 @@ import { Controller, Get, Param, Query } from '@nestjs/common'
 import { ApiOAuth2, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { GetAnimationsCollectivesQueryHandler } from 'src/application/queries/get-animations-collectives.query.handler.db'
 import { AnimationCollectiveQueryModel } from 'src/application/queries/query-models/rendez-vous.query-model'
+import { GetJeunesByEtablissementQueryHandler } from '../../application/queries/get-jeunes-by-etablissement.query.handler.db'
+import { JeuneQueryModel } from '../../application/queries/query-models/jeunes.query-model'
 import { isSuccess } from '../../building-blocks/types/result'
 import { Authentification } from '../../domain/authentification'
+import { DateService } from '../../utils/date-service'
 import { Utilisateur } from '../decorators/authenticated.decorator'
 import { handleFailure } from './failure.handler'
-import { DateService } from '../../utils/date-service'
 import { GetAnimationsCollectivesQueryParams } from './validation/etablissements.inputs'
 
 @Controller('etablissements')
@@ -14,7 +16,8 @@ import { GetAnimationsCollectivesQueryParams } from './validation/etablissements
 @ApiTags('Etablissements')
 export class EtablissementsController {
   constructor(
-    private readonly getAnimationsCollectivesQueryHandler: GetAnimationsCollectivesQueryHandler
+    private readonly getAnimationsCollectivesQueryHandler: GetAnimationsCollectivesQueryHandler,
+    private readonly getJeunesEtablissementQueryHandler: GetJeunesByEtablissementQueryHandler
   ) {}
 
   @ApiOperation({
@@ -43,6 +46,31 @@ export class EtablissementsController {
           getAnimationsCollectivesQueryParams.dateFin
         )
       },
+      utilisateur
+    )
+
+    if (isSuccess(result)) {
+      return result.data
+    }
+
+    throw handleFailure(result)
+  }
+
+  @ApiOperation({
+    summary: "Récupère les jeunes d'un établissement",
+    description: "Autorisé pour un conseiller appartenant à l'établissement"
+  })
+  @Get(':idEtablissement/jeunes')
+  @ApiResponse({
+    type: JeuneQueryModel,
+    isArray: true
+  })
+  async getJeunesDeLEtablissement(
+    @Param('idEtablissement') idEtablissement: string,
+    @Utilisateur() utilisateur: Authentification.Utilisateur
+  ): Promise<JeuneQueryModel[]> {
+    const result = await this.getJeunesEtablissementQueryHandler.execute(
+      { idEtablissement },
       utilisateur
     )
 
