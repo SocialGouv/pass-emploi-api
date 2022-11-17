fac84daeba6c84f90d0a79255edd25d18bb31b0e
Sonny Klotz <klotz.sonny@gmail.com>
2022-11-17 17:20:59 +0100

    test(wip): séparation des domaines dans les tests

--- test/domain/rendez-vous/animation-collective.test.ts
+++ test/domain/rendez-vous/animation-collective.test.ts
@@ -1,105 +1,35 @@
-import { StubbedType, stubInterface } from '@salesforce/ts-sinon'
+import { createSandbox, expect, stubClass } from '../../utils'
+import { IdService } from '../../../src/utils/id-service'
 import {
   CodeTypeRendezVous,
-  InfosRendezVousACreer,
   RendezVous
-} from 'src/domain/rendez-vous/rendez-vous'
-import { IdService } from 'src/utils/id-service'
-import { unConseiller } from 'test/fixtures/conseiller.fixture'
-import { uneDatetime } from 'test/fixtures/date.fixture'
-import { unJeune } from 'test/fixtures/jeune.fixture'
-import { DateService } from '../../src/utils/date-service'
+} from '../../../src/domain/rendez-vous/rendez-vous'
+import { uneDatetime } from '../../fixtures/date.fixture'
+import { unJeune } from '../../fixtures/jeune.fixture'
+import { StubbedType, stubInterface } from '@salesforce/ts-sinon'
+import { DateService } from '../../../src/utils/date-service'
 import {
   uneAnimationCollective,
   unJeuneDuRendezVous,
   unRendezVous
-} from '../fixtures/rendez-vous.fixture'
-import { createSandbox, expect, stubClass } from '../utils'
-import { failure, success } from '../../src/building-blocks/types/result'
-import { MauvaiseCommandeError } from '../../src/building-blocks/types/domain-error'
+} from '../../fixtures/rendez-vous.fixture'
+import { failure, success } from '../../../src/building-blocks/types/result'
+import { MauvaiseCommandeError } from '../../../src/building-blocks/types/domain-error'
 
-describe('Rendez-vous', () => {
+describe('AnimationCollective', () => {
   const id = '26279b34-318a-45e4-a8ad-514a1090462c'
   const idService = stubClass(IdService)
   idService.uuid.returns(id)
 
-  describe('createRendezVousConseiller', () => {
-    describe('quand le type est autre que animation collective', () => {
-      // Given
-      const infosRdv: InfosRendezVousACreer = {
-        idsJeunes: ['1'],
-        idConseiller: '41',
-        commentaire: '',
-        date: uneDatetime().toJSDate().toISOString(),
-        duree: 10
-      }
-      const conseiller = unConseiller()
-
-      // When
-      const rendezVous = RendezVous.createRendezVousConseiller(
-        infosRdv,
-        [unJeune()],
-        conseiller,
-        idService
-      )
-
-      it('renvoie un rdv avec titre et sousTitre', async () => {
-        expect(rendezVous.sousTitre).to.equal('avec Nils')
-        expect(rendezVous.titre).to.equal('Rendez-vous conseiller')
-      })
-      it('renvoie un rdv avec type par défaut', async () => {
-        expect(rendezVous.type).to.equal(
-          CodeTypeRendezVous.ENTRETIEN_INDIVIDUEL_CONSEILLER
-        )
-      })
-      it('renvoie un rdv avec presenceConseiller par défaut', async () => {
-        expect(rendezVous.presenceConseiller).to.equal(true)
-      })
-      it('renvoie un rdv avec le bon créateur', async () => {
-        expect(rendezVous.createur).to.deep.equal({
-          id: conseiller.id,
-          nom: conseiller.lastName,
-          prenom: conseiller.firstName
-        })
-      })
-    })
-    describe('quand le type est animation collective', () => {
-      // Given
-      const infosRdv: InfosRendezVousACreer = {
-        idsJeunes: ['1'],
-        idConseiller: '41',
-        commentaire: '',
-        date: uneDatetime().toJSDate().toISOString(),
-        duree: 10,
-        type: CodeTypeRendezVous.INFORMATION_COLLECTIVE
-      }
-      const conseiller = unConseiller({ agence: { id: 'test' } })
-
-      // When
-      const rendezVous = RendezVous.createRendezVousConseiller(
-        infosRdv,
-        [unJeune()],
-        conseiller,
-        idService
-      )
-
-      it('renvoie un rdv avec agence', async () => {
-        expect(rendezVous.idAgence).to.equal('test')
-      })
-    })
-  })
-
-  describe('AnimationCollective.Service', () => {
+  describe('Service', () => {
     const maintenant = uneDatetime()
     let service: RendezVous.AnimationCollective.Service
     let repository: StubbedType<RendezVous.AnimationCollective.Repository>
-    let rdvRepository: StubbedType<RendezVous.Repository>
     let dateService: StubbedType<DateService>
 
     beforeEach(() => {
       const sandbox = createSandbox()
       repository = stubInterface(sandbox)
-      rdvRepository = stubInterface(sandbox)
       dateService = stubClass(DateService)
       dateService.now.returns(maintenant)
       service = new RendezVous.AnimationCollective.Service(
@@ -128,7 +58,7 @@ describe('Rendez-vous', () => {
           type: CodeTypeRendezVous.INFORMATION_COLLECTIVE,
           jeunes: [unJeune({ id: '2' })]
         })
-        expect(rdvRepository.save).to.have.been.calledWithExactly(
+        expect(repository.save).to.have.been.calledWithExactly(
           uneAnimationCollectiveMiseAJour
         )
       })
