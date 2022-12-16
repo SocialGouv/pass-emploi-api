572c2ee90352d4706e14e4501b8a18fc0b33edba
Joseph Robert <joseph.robert@octo.com>
2022-12-16 10:36:33 +0100

    feat(1274): notifier les jeunes

--- test/application/commands/envoyer-message-groupe.command.handler.test.ts
+++ test/application/commands/envoyer-message-groupe.command.handler.test.ts
@@ -9,33 +9,49 @@ import { AuthorizeConseillerForJeunes } from '../../../src/application/authorize
 import { unUtilisateurConseiller } from '../../fixtures/authentification.fixture'
 import TypeMessage = Chat.TypeMessage
 import { Evenement, EvenementService } from '../../../src/domain/evenement'
+import { Notification } from '../../../src/domain/notification/notification'
+import { Jeune } from '../../../src/domain/jeune/jeune'
+import { unJeune } from '../../fixtures/jeune.fixture'
 
 describe('EnvoyerMessageGroupeCommandHandler', () => {
   let envoyerMessageGroupeCommandHandler: EnvoyerMessageGroupeCommandHandler
   let authorizeConseillerForJeunes: StubbedClass<AuthorizeConseillerForJeunes>
   let evenementService: StubbedClass<EvenementService>
+  let notificationService: StubbedClass<Notification.Service>
   let chatRepository: StubbedType<Chat.Repository>
+  let jeuneRepository: StubbedType<Jeune.Repository>
 
   beforeEach(() => {
-    chatRepository = stubInterface(createSandbox())
+    const sandbox = createSandbox()
+    chatRepository = stubInterface(sandbox)
+    jeuneRepository = stubInterface(sandbox)
     authorizeConseillerForJeunes = stubClass(AuthorizeConseillerForJeunes)
     evenementService = stubClass(EvenementService)
+    notificationService = stubClass(Notification.Service)
     envoyerMessageGroupeCommandHandler = new EnvoyerMessageGroupeCommandHandler(
       chatRepository,
+      jeuneRepository,
       authorizeConseillerForJeunes,
-      evenementService
+      evenementService,
+      notificationService
     )
   })
 
   describe('handle', () => {
     describe('quand le message est sans PJ', () => {
-      it('envoie un message à chaque jeune', async () => {
+      const jeunes = [unJeune({ id: 'jeune-1' }), unJeune({ id: 'jeune-2' })]
+
+      beforeEach(async () => {
         // Given
         chatRepository.recupererChat
           .withArgs('jeune-1')
           .resolves({ id: 'chat-1' })
           .withArgs('jeune-2')
           .resolves({ id: 'chat-2' })
+
+        jeuneRepository.findAllJeunesByConseiller
+          .withArgs(['jeune-1', 'jeune-2'], '41')
+          .resolves(jeunes)
 
         // When
         await envoyerMessageGroupeCommandHandler.handle({
@@ -44,7 +60,8 @@ describe('EnvoyerMessageGroupeCommandHandler', () => {
           iv: '123456',
           idConseiller: '41'
         })
-
+      })
+      it('envoie un message à chaque jeune', async () => {
         // Then
         expect(chatRepository.envoyerMessageBeneficiaire).to.have.callCount(2)
         expect(
@@ -65,6 +82,12 @@ describe('EnvoyerMessageGroupeCommandHandler', () => {
           type: TypeMessage.MESSAGE,
           infoPieceJointe: undefined
         })
+      })
+      it('notifie chaque jeune', async () => {
+        // Then
+        expect(
+          notificationService.notifierLesJeunesDuNouveauMessage
+        ).to.have.been.calledWithExactly(jeunes)
       })
     })
     describe('quand le message est avec PJ', () => {
