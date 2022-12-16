10fda4b7241b3d0f340d9e8ec7095c02e0cea353
Joseph Robert <joseph.robert@octo.com>
2022-12-16 09:53:26 +0100

    feat(1274): événements d'engagement

--- test/application/commands/envoyer-message-groupe.command.handler.test.ts
+++ test/application/commands/envoyer-message-groupe.command.handler.test.ts
@@ -1,19 +1,31 @@
 import { StubbedType, stubInterface } from '@salesforce/ts-sinon'
-import { EnvoyerMessageGroupeCommandHandler } from '../../../src/application/commands/envoyer-message-groupe.command.handler'
+import {
+  EnvoyerMessageGroupeCommand,
+  EnvoyerMessageGroupeCommandHandler
+} from '../../../src/application/commands/envoyer-message-groupe.command.handler'
 import { Chat } from '../../../src/domain/chat'
-import { createSandbox, expect } from '../../utils'
+import { createSandbox, expect, StubbedClass, stubClass } from '../../utils'
 import { failure } from '../../../src/building-blocks/types/result'
 import { MauvaiseCommandeError } from '../../../src/building-blocks/types/domain-error'
+import { AuthorizeConseillerForJeunes } from '../../../src/application/authorizers/authorize-conseiller-for-jeunes'
+import { unUtilisateurConseiller } from '../../fixtures/authentification.fixture'
 import TypeMessage = Chat.TypeMessage
+import { Evenement, EvenementService } from '../../../src/domain/evenement'
 
 describe('EnvoyerMessageGroupeCommandHandler', () => {
   let envoyerMessageGroupeCommandHandler: EnvoyerMessageGroupeCommandHandler
+  let authorizeConseillerForJeunes: StubbedClass<AuthorizeConseillerForJeunes>
+  let evenementService: StubbedClass<EvenementService>
   let chatRepository: StubbedType<Chat.Repository>
 
   beforeEach(() => {
     chatRepository = stubInterface(createSandbox())
+    authorizeConseillerForJeunes = stubClass(AuthorizeConseillerForJeunes)
+    evenementService = stubClass(EvenementService)
     envoyerMessageGroupeCommandHandler = new EnvoyerMessageGroupeCommandHandler(
-      chatRepository
+      chatRepository,
+      authorizeConseillerForJeunes,
+      evenementService
     )
   })
 
@@ -26,8 +38,6 @@ describe('EnvoyerMessageGroupeCommandHandler', () => {
           .resolves({ id: 'chat-1' })
           .withArgs('jeune-2')
           .resolves({ id: 'chat-2' })
-        const envoyerMessageGroupeCommandHandler =
-          new EnvoyerMessageGroupeCommandHandler(chatRepository)
 
         // When
         await envoyerMessageGroupeCommandHandler.handle({
@@ -77,6 +87,129 @@ describe('EnvoyerMessageGroupeCommandHandler', () => {
             new MauvaiseCommandeError('Un message PJ doit avoir des info de PJ')
           )
         )
+      })
+    })
+  })
+
+  describe('authorize', () => {
+    it('autorise un conseiller à envoyer un message à ses jeunes', () => {
+      // Given
+      const command: EnvoyerMessageGroupeCommand = {
+        idsBeneficiaires: ['jeune-1', 'jeune-2'],
+        message: 'un message',
+        iv: '123456',
+        idConseiller: '41',
+        typeMessage: TypeMessage.MESSAGE
+      }
+      const utilisateur = unUtilisateurConseiller()
+
+      // When
+      envoyerMessageGroupeCommandHandler.authorize(command, utilisateur)
+
+      // Then
+      expect(
+        authorizeConseillerForJeunes.authorize
+      ).to.have.been.calledWithExactly(command.idsBeneficiaires, utilisateur)
+    })
+  })
+
+  describe('monitor', () => {
+    const utilisateur = unUtilisateurConseiller()
+
+    describe("quand c'est un message sans pièce jointe", () => {
+      describe('envoyé à un seul jeune', () => {
+        it('sauvegarde un événement MESSAGE_ENVOYE', async () => {
+          // Given
+          const command: EnvoyerMessageGroupeCommand = {
+            idsBeneficiaires: ['jeune-1'],
+            message: 'un message',
+            iv: '123456',
+            idConseiller: '41',
+            typeMessage: TypeMessage.MESSAGE
+          }
+
+          // When
+          await envoyerMessageGroupeCommandHandler.monitor(utilisateur, command)
+
+          // Then
+          expect(evenementService.creer).to.have.been.calledWithExactly(
+            Evenement.Code.MESSAGE_ENVOYE,
+            utilisateur
+          )
+        })
+      })
+      describe('envoyé à plusieurs jeunes', () => {
+        it('sauvegarde un événement MESSAGE_ENVOYE_MULTIPLE', async () => {
+          // Given
+          const command: EnvoyerMessageGroupeCommand = {
+            idsBeneficiaires: ['jeune-1', 'jeune-2'],
+            message: 'un message',
+            iv: '123456',
+            idConseiller: '41',
+            typeMessage: TypeMessage.MESSAGE
+          }
+
+          // When
+          await envoyerMessageGroupeCommandHandler.monitor(utilisateur, command)
+
+          // Then
+          expect(evenementService.creer).to.have.been.calledWithExactly(
+            Evenement.Code.MESSAGE_ENVOYE_MULTIPLE,
+            utilisateur
+          )
+        })
+      })
+    })
+    describe("quand c'est un message avec pièce jointe", () => {
+      describe('envoyé à un seul jeune', () => {
+        it('sauvegarde un événement MESSAGE_ENVOYE_PJ', async () => {
+          // Given
+          const command: EnvoyerMessageGroupeCommand = {
+            idsBeneficiaires: ['jeune-1'],
+            message: 'un message',
+            iv: '123456',
+            idConseiller: '41',
+            typeMessage: TypeMessage.MESSAGE_PJ,
+            infoPieceJointe: {
+              id: 'id',
+              nom: 'nom'
+            }
+          }
+
+          // When
+          await envoyerMessageGroupeCommandHandler.monitor(utilisateur, command)
+
+          // Then
+          expect(evenementService.creer).to.have.been.calledWithExactly(
+            Evenement.Code.MESSAGE_ENVOYE_PJ,
+            utilisateur
+          )
+        })
+      })
+      describe('envoyé à plusieurs jeunes', () => {
+        it('sauvegarde un événement MESSAGE_ENVOYE_MULTIPLE_PJ', async () => {
+          // Given
+          const command: EnvoyerMessageGroupeCommand = {
+            idsBeneficiaires: ['jeune-1', 'jeune-2'],
+            message: 'un message',
+            iv: '123456',
+            idConseiller: '41',
+            typeMessage: TypeMessage.MESSAGE_PJ,
+            infoPieceJointe: {
+              id: 'id',
+              nom: 'nom'
+            }
+          }
+
+          // When
+          await envoyerMessageGroupeCommandHandler.monitor(utilisateur, command)
+
+          // Then
+          expect(evenementService.creer).to.have.been.calledWithExactly(
+            Evenement.Code.MESSAGE_ENVOYE_MULTIPLE_PJ,
+            utilisateur
+          )
+        })
       })
     })
   })
