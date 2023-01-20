bb893bca628e497159e6bcc57d8360130d3eb775
Joseph Robert <joseph.robert@octo.com>
2023-01-20 11:29:24 +0100

    docs: swagger api fichier cej 2

--- src/infrastructure/routes/validation/support.input.ts
+++ src/infrastructure/routes/validation/support.input.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { ValidateIf } from 'class-validator'
 
 export class TeleverserCsvPayload {
   @ApiProperty({ type: 'string', format: 'binary' })
+  @ValidateIf(() => false)
   fichier: Express.Multer.File
 }
