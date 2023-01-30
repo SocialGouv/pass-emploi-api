0c3c8f0231eccb3acffc2a32592c16cbc5ae978e
Sonny Klotz <klotz.sonny@gmail.com>
2023-01-30 17:41:55 +0100

    refactor: log pour le rate-limiter

--- src/infrastructure/repositories/partenaire/milo/milo-rendez-vous-http.repository.ts
+++ src/infrastructure/repositories/partenaire/milo/milo-rendez-vous-http.repository.ts
@@ -37,6 +37,7 @@ export class MiloRendezVousHttpRepository implements MiloRendezVous.Repository {
 
   async findAllEvenements(): Promise<MiloRendezVous.Evenement[]> {
     await this.rateLimiterService.getEvenementMilo.attendreLaProchaineDisponibilite()
+    this.logger.log("Appel de l'api i-milo findAllEvenements")
     const evenements = await firstValueFrom(
       this.httpService.get<EvenementMiloDto[]>(
         `${this.apiUrl}/operateurs/events`,
