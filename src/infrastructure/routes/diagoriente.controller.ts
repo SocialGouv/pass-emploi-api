241e7223ab50bfccd73bcd6810a0e50ed93dd498
Malek Zemni <malek.zemni@octo.com>
2023-02-16 14:57:51 +0100

    feat(diagoriente): refacto query model des urls

--- src/infrastructure/routes/diagoriente.controller.ts
+++ src/infrastructure/routes/diagoriente.controller.ts
@@ -1,10 +1,9 @@
 import { Controller, Get, Param } from '@nestjs/common'
 import { ApiOAuth2, ApiResponse, ApiTags } from '@nestjs/swagger'
 import {
-  DiagorienteUrlQueryModel,
-  GetDiagorienteUrlQueryHandler,
-  TypeUrlDiagoriente
-} from '../../application/queries/get-diagoriente-url.query.handler.db'
+  DiagorienteUrlsQueryModel,
+  GetDiagorienteUrlsQueryHandler
+} from '../../application/queries/get-diagoriente-urls.query.handler.db'
 import { isSuccess } from '../../building-blocks/types/result'
 import { Authentification } from '../../domain/authentification'
 import { Utilisateur } from '../decorators/authenticated.decorator'
@@ -15,43 +14,20 @@ import { handleFailure } from './failure.handler'
 @ApiTags('Diagoriente')
 export class DiagorienteController {
   constructor(
-    private readonly getDiagorienteUrlQueryHandler: GetDiagorienteUrlQueryHandler
+    private readonly getDiagorienteUrlsQueryHandler: GetDiagorienteUrlsQueryHandler
   ) {}
 
-  @Get('urls/chatbot')
+  @Get('urls')
   @ApiResponse({
-    type: DiagorienteUrlQueryModel
+    type: DiagorienteUrlsQueryModel
   })
   async getDiagorienteUrlChatbot(
     @Param('idJeune') idJeune: string,
     @Utilisateur() utilisateur: Authentification.Utilisateur
-  ): Promise<DiagorienteUrlQueryModel> {
-    const result = await this.getDiagorienteUrlQueryHandler.execute(
+  ): Promise<DiagorienteUrlsQueryModel> {
+    const result = await this.getDiagorienteUrlsQueryHandler.execute(
       {
-        idJeune,
-        typeUrl: TypeUrlDiagoriente.CHATBOT
-      },
-      utilisateur
-    )
-
-    if (isSuccess(result)) {
-      return result.data
-    }
-    throw handleFailure(result)
-  }
-
-  @Get('urls/metiers-favoris')
-  @ApiResponse({
-    type: DiagorienteUrlQueryModel
-  })
-  async getDiagorienteUrlFavoris(
-    @Param('idJeune') idJeune: string,
-    @Utilisateur() utilisateur: Authentification.Utilisateur
-  ): Promise<DiagorienteUrlQueryModel> {
-    const result = await this.getDiagorienteUrlQueryHandler.execute(
-      {
-        idJeune,
-        typeUrl: TypeUrlDiagoriente.FAVORIS
+        idJeune
       },
       utilisateur
     )
