a6929260a81b1301270e7751a7393826660e1c3b
Malek Zemni <malek.zemni@octo.com>
2023-03-01 11:51:46 +0100

    v2.17.5

--- src/application/queries/get-metiers-rome.query.handler.db.ts
+++ src/application/queries/get-metiers-rome.query.handler.db.ts
@@ -28,8 +28,8 @@ export class GetMetiersRomeQueryHandler extends QueryHandler<
     const metiers: CommuneSqlModel[] = await this.sequelize.query(
       `SELECT code, libelle, SIMILARITY(libelle_sanitized, ?) AS "score"
        FROM "referentiel_metier_rome"
-       WHERE SIMILARITY(libelle_sanitized, ?) > 0 OR code = ?
-       ORDER BY "score" DESC;`,
+       WHERE SIMILARITY(libelle_sanitized, ?) > 0.1 OR code = ?
+       ORDER BY "score" DESC LIMIT 20;`,
       {
         replacements: [
           sanitizedRecherche,
