941f00d3c55b091174ac9a7abb76cc681c5b39b4
Joseph Robert <joseph.robert@octo.com>
2023-03-30 09:23:26 +0200

    opseries: charger les événements d'engagement en différentiel via un stream assez bas niveau

--- scripts/analytics/0_db_dump_restore.sh
+++ scripts/analytics/0_db_dump_restore.sh
@@ -33,6 +33,7 @@ pg_dump --clean --if-exists --format c --dbname "$DUMP_RESTORE_DB_SOURCE" --no-o
   --exclude-table 'spatial_ref_sys' \
   --exclude-table 'log_api_partenaire' \
   --exclude-table 'suivi_job' \
+  --exclude-table 'evenement_engagement' \
   --exclude-table 'evenement_engagement_hebdo'
 
 if [ $? -ne 0 ]; then
@@ -45,16 +46,9 @@ psql -d ${DUMP_RESTORE_DB_TARGET} \
   -c "CREATE EXTENSION IF NOT EXISTS postgis SCHEMA public;" \
   -c "CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder CASCADE SCHEMA public;" \
   -c "CREATE EXTENSION IF NOT EXISTS postgis_topology SCHEMA topology;" \
-  -c "CREATE EXTENSION IF NOT EXISTS pg_trgm SCHEMA public;" \
-  -c "drop index IF EXISTS evenement_engagement_date_evenement_index;" \
-  -c "drop index IF EXISTS evenement_engagement_categorie_index;" \
-  -c "drop index IF EXISTS evenement_engagement_action_index;" \
-  -c "drop index IF EXISTS evenement_engagement_nom_index;" \
-  -c "drop index IF EXISTS evenement_engagement_id_utilisateur_index;" \
-  -c "drop index IF EXISTS evenement_engagement_type_utilisateur_index;" \
-  -c "drop index IF EXISTS evenement_engagement_structure_index;" \
-  -c "drop index IF EXISTS evenement_engagement_code_index;"
-echo "extensions crées et index supprimés"
+  -c "CREATE EXTENSION IF NOT EXISTS pg_trgm SCHEMA public;"
+
+echo "extensions crées"
 
 pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname "${DUMP_RESTORE_DB_TARGET}" dump.pgsql
 
@@ -64,19 +58,3 @@ fi
 echo "pg_restore OK"
 
 rm -f dump.pgsql
-
-psql -d ${DUMP_RESTORE_DB_TARGET} \
-  -c "create index evenement_engagement_date_evenement_index on evenement_engagement (date_evenement);" \
-  -c "create index evenement_engagement_categorie_index on evenement_engagement (categorie);" \
-  -c "create index evenement_engagement_action_index on evenement_engagement (action);" \
-  -c "create index evenement_engagement_nom_index on evenement_engagement (nom);" \
-  -c "create index evenement_engagement_id_utilisateur_index on evenement_engagement (id_utilisateur);" \
-  -c "create index evenement_engagement_type_utilisateur_index on evenement_engagement (type_utilisateur);" \
-  -c "create index evenement_engagement_structure_index on evenement_engagement (structure);" \
-  -c "create index evenement_engagement_code_index on evenement_engagement (code);"
-
-if [ $? -ne 0 ]; then
-  echo "Error: index creation failed"
-  exit 1
-fi
-echo "index creation OK"
---
+++ src/application/jobs/analytics/1-charger-les-evenements.job.ts
@@ -0,0 +1,139 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { JobHandler } from '../../../building-blocks/types/job-handler'
+import {
+  Planificateur,
+  PlanificateurRepositoryToken,
+  ProcessJobType
+} from '../../../domain/planificateur'
+import { SuiviJob, SuiviJobServiceToken } from '../../../domain/suivi-job'
+import { DateService } from '../../../utils/date-service'
+import {
+  getConnexionToDBSource,
+  getConnexionToDBTarget
+} from '../../../infrastructure/sequelize/connector-analytics'
+import { Sequelize } from 'sequelize-typescript'
+import { SequelizeInjectionToken } from '../../../infrastructure/sequelize/providers'
+import { PoolClient } from 'pg'
+import { from as copyFrom, to as copyTo } from 'pg-copy-streams'
+import { pipeline } from 'node:stream/promises'
+
+@Injectable()
+@ProcessJobType(Planificateur.JobType.CHARGER_EVENEMENTS_ANALYTICS)
+export class ChargerEvenementsJobHandler extends JobHandler<Planificateur.Job> {
+  constructor(
+    @Inject(SuiviJobServiceToken)
+    suiviJobService: SuiviJob.Service,
+    private dateService: DateService,
+    @Inject(PlanificateurRepositoryToken)
+    private planificateurRepository: Planificateur.Repository,
+    @Inject(SequelizeInjectionToken)
+    private readonly sequelizeSource: Sequelize
+  ) {
+    super(Planificateur.JobType.CHARGER_EVENEMENTS_ANALYTICS, suiviJobService)
+  }
+
+  async handle(_job: Planificateur.Job): Promise<SuiviJob> {
+    let erreur
+    const maintenant = this.dateService.now()
+
+    const connexionSource = await getConnexionToDBSource()
+    const connexionTarget = await getConnexionToDBTarget()
+
+    await this.mettreAJourLeSchema(connexionTarget.client)
+    await this.indexerLesColonnes(connexionTarget.client)
+    await this.ajouterLesNouveauxEvenements(
+      connexionSource.client,
+      connexionTarget.client
+    )
+
+    const jobEnrichirLesEvenements: Planificateur.Job<void> = {
+      dateExecution: this.dateService.nowJs(),
+      type: Planificateur.JobType.ENRICHIR_EVENEMENTS_ANALYTICS,
+      contenu: undefined
+    }
+    await this.planificateurRepository.creerJob(jobEnrichirLesEvenements)
+
+    await connexionSource.close()
+    await connexionTarget.close()
+
+    return {
+      jobType: this.jobType,
+      nbErreurs: 0,
+      succes: erreur ? false : true,
+      dateExecution: maintenant,
+      tempsExecution: DateService.calculerTempsExecution(maintenant),
+      resultat: {}
+    }
+  }
+
+  private async mettreAJourLeSchema(clientTarget: PoolClient): Promise<void> {
+    await clientTarget.query(`
+      CREATE TABLE IF NOT EXISTS evenement_engagement
+      (
+        id               serial,
+        date_evenement   timestamp with time zone,
+        categorie        varchar(255),
+        action           varchar(255),
+        nom              varchar(255),
+        id_utilisateur   varchar(255),
+        type_utilisateur varchar(255),
+        structure        varchar(255),
+        code             varchar(255)
+      );
+    `)
+  }
+
+  private async indexerLesColonnes(clientTarget: PoolClient): Promise<void> {
+    await clientTarget.query(`
+      create index if not exists evenement_engagement_date_evenement_index on evenement_engagement (date_evenement);
+      create index if not exists evenement_engagement_categorie_index on evenement_engagement (categorie);
+      create index if not exists evenement_engagement_action_index on evenement_engagement (action);
+      create index if not exists evenement_engagement_nom_index on evenement_engagement (nom);
+      create index if not exists evenement_engagement_id_utilisateur_index on evenement_engagement (id_utilisateur);
+      create index if not exists evenement_engagement_type_utilisateur_index on evenement_engagement (type_utilisateur);
+      create index if not exists evenement_engagement_structure_index on evenement_engagement (structure);
+      create index if not exists evenement_engagement_code_index on evenement_engagement (code);
+    `)
+  }
+
+  private async ajouterLesNouveauxEvenements(
+    clientSource: PoolClient,
+    clientTarget: PoolClient
+  ): Promise<void> {
+    const dernierEvenementCharge = await this.getDernierEvenementCharge(
+      clientTarget
+    )
+    this.logger.log(`dernier id evenement chargé: ${dernierEvenementCharge}`)
+
+    const streamCopyTo = await clientSource.query(
+      copyTo(
+        `COPY (SELECT * FROM evenement_engagement WHERE id>${dernierEvenementCharge}) TO STDOUT WITH NULL '\\LA_VALEUR_NULL'`
+      )
+    )
+
+    const streamCopyFrom = clientTarget.query(
+      copyFrom(
+        `COPY evenement_engagement (id,
+                           date_evenement,
+                           categorie,
+                           action,
+                           nom,
+                           id_utilisateur,
+                           type_utilisateur,
+                           structure,
+                           code) FROM STDIN WITH NULL AS '\\LA_VALEUR_NULL'`
+      )
+    )
+    await pipeline(streamCopyTo, streamCopyFrom)
+  }
+
+  private async getDernierEvenementCharge(
+    clientTarget: PoolClient
+  ): Promise<number> {
+    const result = await clientTarget.query(
+      `SELECT MAX(id) as max
+       from evenement_engagement;`
+    )
+    return result.rows[0].max ?? 0
+  }
+}
