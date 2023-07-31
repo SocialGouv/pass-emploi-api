096f630a72d0a51a6f032d3b25cfa4b442ac1c7a
gabriel.adgeg <gad@octo.com>
2023-07-31 17:56:54 +0200

    tech(session): Remove useless transaction in migration

--- src/infrastructure/sequelize/migrations/20230731142459-ajout-date-cloture-session-milo.js
+++ src/infrastructure/sequelize/migrations/20230731142459-ajout-date-cloture-session-milo.js
@@ -2,24 +2,13 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.sequelize.transaction(async transaction => {
-      await queryInterface.addColumn(
-        'session_milo',
-        'date_cloture',
-        {
-          type: Sequelize.DATE,
-          allowNull: true
-        },
-        { transaction }
-      )
+    await queryInterface.addColumn('session_milo', 'date_cloture', {
+      type: Sequelize.DATE,
+      allowNull: true
     })
   },
 
   async down(queryInterface) {
-    await queryInterface.sequelize.transaction(async transaction => {
-      await queryInterface.removeColumn('session_milo', 'date_cloture', {
-        transaction
-      })
-    })
+    await queryInterface.removeColumn('session_milo', 'date_cloture')
   }
 }
