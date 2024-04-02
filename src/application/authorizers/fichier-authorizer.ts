112a174bfafa703afb180330076e7a4f6e086a26
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2024-04-02 14:29:08 +0200

    feat: permet televersement PJ beneficiaire

--- src/application/authorizers/fichier-authorizer.ts
+++ src/application/authorizers/fichier-authorizer.ts
@@ -1,8 +1,11 @@
 import { Inject, Injectable } from '@nestjs/common'
+import { ConseillerAuthorizer } from 'src/application/authorizers/conseiller-authorizer'
+import { ListeDeDiffusionAuthorizer } from 'src/application/authorizers/liste-de-diffusion-authorizer'
 import { DroitsInsuffisants } from '../../building-blocks/types/domain-error'
 import {
   emptySuccess,
   failure,
+  isFailure,
   Result
 } from '../../building-blocks/types/result'
 import { Authentification } from '../../domain/authentification'
@@ -15,8 +18,42 @@ export class FichierAuthorizer {
     @Inject(FichierRepositoryToken)
     private fichierRepository: Fichier.Repository,
     @Inject(JeuneRepositoryToken)
-    private jeuneRepository: Jeune.Repository
+    private jeuneRepository: Jeune.Repository,
+    private conseillerAuthorizer: ConseillerAuthorizer,
+    private listeDiffusionAuthorizer: ListeDeDiffusionAuthorizer
   ) {}
+
+  async autoriserTeleversementDuFichier(
+    utilisateur: Authentification.Utilisateur,
+    idsJeunes?: string[],
+    idsListeDiffusion?: string[]
+  ): Promise<Result> {
+    if (utilisateur.type === Authentification.Type.JEUNE) return emptySuccess()
+
+    if (idsJeunes?.length) {
+      const resultJeunes =
+        await this.conseillerAuthorizer.autoriserConseillerPourSesJeunes(
+          idsJeunes,
+          utilisateur
+        )
+      if (isFailure(resultJeunes)) return resultJeunes
+    }
+
+    if (idsListeDiffusion?.length) {
+      for (const idListe of idsListeDiffusion) {
+        const result =
+          await this.listeDiffusionAuthorizer.autoriserConseillerPourSaListeDeDiffusion(
+            idListe,
+            utilisateur
+          )
+        if (isFailure(result)) {
+          return result
+        }
+      }
+    }
+
+    return emptySuccess()
+  }
 
   async autoriserTelechargementDuFichier(
     idFichier: string,
@@ -25,27 +62,37 @@ export class FichierAuthorizer {
     const fichierMetadata = await this.fichierRepository.getFichierMetadata(
       idFichier
     )
+
     if (fichierMetadata) {
-      if (utilisateur.type === Authentification.Type.JEUNE) {
-        if (fichierMetadata.idsJeunes.includes(utilisateur.id)) {
-          return emptySuccess()
-        }
-      }
+      if (fichierMetadata.idCreateur === utilisateur.id) return emptySuccess()
+
+      if (
+        utilisateur.type === Authentification.Type.JEUNE &&
+        fichierMetadata.idsJeunes.includes(utilisateur.id)
+      )
+        return emptySuccess()
+
       if (utilisateur.type === Authentification.Type.CONSEILLER) {
-        const jeunesDuFichier =
-          await this.jeuneRepository.findAllJeunesByIdsAndConseiller(
-            fichierMetadata.idsJeunes,
-            utilisateur.id
-          )
-        const leConseillerADesJeunesDansLeFichier = jeunesDuFichier.length > 0
+        const jeunesDuConseiller =
+          await this.jeuneRepository.findAllJeunesByConseiller(utilisateur.id)
+        const idsJeunesDuConseiller = jeunesDuConseiller.map(({ id }) => id)
+
         if (
-          leConseillerADesJeunesDansLeFichier ||
-          fichierMetadata.idCreateur === utilisateur.id
-        ) {
+          fichierMetadata.typeCreateur === Authentification.Type.JEUNE &&
+          idsJeunesDuConseiller.includes(fichierMetadata.idCreateur)
+        )
           return emptySuccess()
+
+        if (fichierMetadata.typeCreateur === Authentification.Type.CONSEILLER) {
+          const leConseillerADesJeunesDansLeFichier =
+            fichierMetadata.idsJeunes.some(idJeuneDuFichier =>
+              idsJeunesDuConseiller.includes(idJeuneDuFichier)
+            )
+          if (leConseillerADesJeunesDansLeFichier) return emptySuccess()
         }
       }
     }
+
     return failure(new DroitsInsuffisants())
   }
 
@@ -56,11 +103,9 @@ export class FichierAuthorizer {
     const fichierMetadata = await this.fichierRepository.getFichierMetadata(
       idFichier
     )
-    if (fichierMetadata) {
-      if (utilisateur.id === fichierMetadata.idCreateur) {
-        return emptySuccess()
-      }
-    }
+    if (fichierMetadata && utilisateur.id === fichierMetadata.idCreateur)
+      return emptySuccess()
+
     return failure(new DroitsInsuffisants())
   }
 }
--- test/application/authorizers/fichier-authorizer.test.ts
+++ test/application/authorizers/fichier-authorizer.test.ts
@@ -1,6 +1,9 @@
 import { StubbedType, stubInterface } from '@salesforce/ts-sinon'
+import { ConseillerAuthorizer } from 'src/application/authorizers/conseiller-authorizer'
+import { ListeDeDiffusionAuthorizer } from 'src/application/authorizers/liste-de-diffusion-authorizer'
 import { DroitsInsuffisants } from 'src/building-blocks/types/domain-error'
 import { emptySuccess, failure } from 'src/building-blocks/types/result'
+import { Authentification } from 'src/domain/authentification'
 import { Fichier } from 'src/domain/fichier'
 import { Jeune } from 'src/domain/jeune/jeune'
 import { unJeune } from 'test/fixtures/jeune.fixture'
@@ -10,25 +13,69 @@ import {
   unUtilisateurJeune
 } from '../../fixtures/authentification.fixture'
 import { unFichierMetadata } from '../../fixtures/fichier.fixture'
-import { createSandbox, expect } from '../../utils'
+import { createSandbox, expect, StubbedClass, stubClass } from '../../utils'
 
 describe('FichierTelechargementAuthorizer', () => {
   let fichierRepository: StubbedType<Fichier.Repository>
   let jeuneRepository: StubbedType<Jeune.Repository>
   let fichierAuthorizer: FichierAuthorizer
+  let authorizeConseillerForJeunes: StubbedClass<ConseillerAuthorizer>
+  let authorizeListeDeDiffusion: StubbedClass<ListeDeDiffusionAuthorizer>
 
   beforeEach(() => {
     const sandbox = createSandbox()
     fichierRepository = stubInterface(sandbox)
     jeuneRepository = stubInterface(sandbox)
+    authorizeConseillerForJeunes = stubClass(ConseillerAuthorizer)
+    authorizeListeDeDiffusion = stubClass(ListeDeDiffusionAuthorizer)
+
     fichierAuthorizer = new FichierAuthorizer(
       fichierRepository,
-      jeuneRepository
+      jeuneRepository,
+      authorizeConseillerForJeunes,
+      authorizeListeDeDiffusion
     )
   })
 
   describe('autoriserTelechargementDuFichier', () => {
     const idFichier = 'test'
+    it('autorise un conseiller quand il est le créateur', async () => {
+      //Given
+      const utilisateur = unUtilisateurConseiller()
+      fichierRepository.getFichierMetadata.withArgs(idFichier).resolves(
+        unFichierMetadata({
+          idsJeunes: ['autre-id'],
+          idCreateur: utilisateur.id
+        })
+      )
+
+      // When
+      const result = await fichierAuthorizer.autoriserTelechargementDuFichier(
+        idFichier,
+        utilisateur
+      )
+
+      // Then
+      expect(result).to.deep.equal(emptySuccess())
+    })
+
+    it('autorise un jeune quand il est le créateur', async () => {
+      //Given
+      const utilisateur = unUtilisateurJeune()
+      fichierRepository.getFichierMetadata
+        .withArgs(idFichier)
+        .resolves(unFichierMetadata({ idCreateur: utilisateur.id }))
+
+      // When
+      const result = await fichierAuthorizer.autoriserTelechargementDuFichier(
+        idFichier,
+        utilisateur
+      )
+
+      // Then
+      expect(result).to.deep.equal(emptySuccess())
+    })
+
     it('autorise un conseiller quand un de ses jeunes est présent', async () => {
       //Given
       const utilisateur = unUtilisateurConseiller()
@@ -36,8 +83,8 @@ describe('FichierTelechargementAuthorizer', () => {
       fichierRepository.getFichierMetadata
         .withArgs(idFichier)
         .resolves(unFichierMetadata({ idsJeunes: [idJeuneDuConseiller] }))
-      jeuneRepository.findAllJeunesByIdsAndConseiller
-        .withArgs([idJeuneDuConseiller], utilisateur.id)
+      jeuneRepository.findAllJeunesByConseiller
+        .withArgs(utilisateur.id)
         .resolves([unJeune()])
 
       // When
@@ -49,39 +96,19 @@ describe('FichierTelechargementAuthorizer', () => {
       // Then
       expect(result).to.deep.equal(emptySuccess())
     })
-    it('autorise un conseiller quand il est le créateur', async () => {
-      //Given
-      const utilisateur = unUtilisateurConseiller()
-      fichierRepository.getFichierMetadata.withArgs(idFichier).resolves(
-        unFichierMetadata({
-          idsJeunes: ['autre-id'],
-          idCreateur: utilisateur.id
-        })
-      )
-      jeuneRepository.findAllJeunesByIdsAndConseiller.resolves([])
 
-      // When
-      const result = await fichierAuthorizer.autoriserTelechargementDuFichier(
-        idFichier,
-        utilisateur
-      )
-
-      // Then
-      expect(result).to.deep.equal(emptySuccess())
-    })
     it("n'autorise pas le conseiller quand aucun de ses jeunes n'est présent", async () => {
       //Given
       const utilisateur = unUtilisateurConseiller()
-      const idJeuneDuConseiller = '1'
       fichierRepository.getFichierMetadata.withArgs(idFichier).resolves(
         unFichierMetadata({
-          idsJeunes: [idJeuneDuConseiller],
+          idsJeunes: ['un-autre-jeune'],
           idCreateur: 'un-autre-createur'
         })
       )
-      jeuneRepository.findAllJeunesByIdsAndConseiller
-        .withArgs([idJeuneDuConseiller], utilisateur.id)
-        .resolves([])
+      jeuneRepository.findAllJeunesByConseiller
+        .withArgs(utilisateur.id)
+        .resolves(['id-jeune-conseiller'])
 
       // When
       const result = await fichierAuthorizer.autoriserTelechargementDuFichier(
@@ -92,6 +119,31 @@ describe('FichierTelechargementAuthorizer', () => {
       // Then
       expect(result).to.deep.equal(failure(new DroitsInsuffisants()))
     })
+
+    it('autorise le conseiller quand un de ses jeunes est le créateur', async () => {
+      //Given
+      const utilisateur = unUtilisateurConseiller()
+      fichierRepository.getFichierMetadata.withArgs(idFichier).resolves(
+        unFichierMetadata({
+          idsJeunes: [],
+          idCreateur: 'id-jeune-conseiller',
+          typeCreateur: Authentification.Type.JEUNE
+        })
+      )
+      jeuneRepository.findAllJeunesByConseiller
+        .withArgs(utilisateur.id)
+        .resolves(['id-jeune-conseiller'])
+
+      // When
+      const result = await fichierAuthorizer.autoriserTelechargementDuFichier(
+        idFichier,
+        utilisateur
+      )
+
+      // Then
+      expect(result).to.deep.equal(failure(new DroitsInsuffisants()))
+    })
+
     it('autorise un jeune quand il est présent', async () => {
       //Given
       const utilisateur = unUtilisateurJeune()
@@ -108,6 +160,7 @@ describe('FichierTelechargementAuthorizer', () => {
       // Then
       expect(result).to.deep.equal(emptySuccess())
     })
+
     it("n'autorise pas le jeune quand il n'est pas présent", async () => {
       //Given
       const utilisateur = unUtilisateurJeune()
@@ -124,6 +177,7 @@ describe('FichierTelechargementAuthorizer', () => {
       // Then
       expect(result).to.deep.equal(failure(new DroitsInsuffisants()))
     })
+
     it("retourne Droits Insuffisants quand le fichier n'existe pas", async () => {
       //Given
       const utilisateur = unUtilisateurJeune()
@@ -172,6 +226,67 @@ describe('FichierTelechargementAuthorizer', () => {
       const result = await fichierAuthorizer.autoriserSuppressionDuFichier(
         idFichier,
         utilisateur
+      )
+
+      // Then
+      expect(result).to.deep.equal(failure(new DroitsInsuffisants()))
+    })
+  })
+
+  describe('autoriserTeleversementDuFichier', () => {
+    it('autorise un conseiller pour ses jeunes', async () => {
+      // Given
+      const utilisateur = unUtilisateurConseiller()
+      authorizeConseillerForJeunes.autoriserConseillerPourSesJeunes
+        .withArgs(['id-jeune'], utilisateur)
+        .resolves(emptySuccess())
+
+      // When
+      const result = await fichierAuthorizer.autoriserTeleversementDuFichier(
+        utilisateur,
+        ['id-jeune']
+      )
+
+      // Then
+      expect(result).to.deep.equal(emptySuccess())
+    })
+
+    it('autorise un conseiller pour ses listes de diffusion', async () => {
+      // Given
+      const utilisateur = unUtilisateurConseiller()
+      authorizeListeDeDiffusion.autoriserConseillerPourSaListeDeDiffusion
+        .withArgs('id-liste-diffusion', utilisateur)
+        .resolves(emptySuccess())
+
+      // When
+      const result = await fichierAuthorizer.autoriserTeleversementDuFichier(
+        utilisateur,
+        undefined,
+        ['id-liste-diffusion']
+      )
+
+      // Then
+      expect(result).to.deep.equal(emptySuccess())
+    })
+
+    it('refuse un conseiller pour une liste de diffusion qui ne lui appartient pas', async () => {
+      // Given
+      const utilisateur = unUtilisateurConseiller()
+      authorizeConseillerForJeunes.autoriserConseillerPourSesJeunes
+        .withArgs(['id-jeune'], utilisateur)
+        .resolves(emptySuccess())
+      authorizeListeDeDiffusion.autoriserConseillerPourSaListeDeDiffusion
+        .withArgs('id-liste-diffusion-1', utilisateur)
+        .resolves(emptySuccess())
+      authorizeListeDeDiffusion.autoriserConseillerPourSaListeDeDiffusion
+        .withArgs('id-liste-diffusion-2', utilisateur)
+        .resolves(failure(new DroitsInsuffisants()))
+
+      // When
+      const result = await fichierAuthorizer.autoriserTeleversementDuFichier(
+        utilisateur,
+        ['id-jeune'],
+        ['id-liste-diffusion-1', 'id-liste-diffusion-2']
       )
 
       // Then
