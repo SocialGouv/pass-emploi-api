cbda217548d70c5edfdc47adbeb33bca9a7d72bf
Malek Zemni <Mzem@users.noreply.github.com>
2024-04-17 13:50:49 +0100

    fix(actions): paginer cron qualification (#1451)
    (cherry picked from commit 7fe376047175932c7a31ba102d24b6c19656a476)

--- src/application/jobs/qualifier-actions.job.handler.db.ts
+++ src/application/jobs/qualifier-actions.job.handler.db.ts
@@ -1,6 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common'
 import { Job } from 'bull'
-import { DateTime } from 'luxon'
 import { Op, WhereOptions } from 'sequelize'
 import { JobHandler } from '../../building-blocks/types/job-handler'
 import { isFailure } from '../../building-blocks/types/result'
@@ -10,7 +9,9 @@ import { SuiviJob, SuiviJobServiceToken } from '../../domain/suivi-job'
 import { ActionSqlRepository } from '../../infrastructure/repositories/action/action-sql.repository.db'
 import { ActionSqlModel } from '../../infrastructure/sequelize/models/action.sql-model'
 import { DateService } from '../../utils/date-service'
+import { DateTime } from 'luxon'
 
+const MAX_ACTIONS = 1000
 @Injectable()
 @ProcessJobType(Planificateur.JobType.QUALIFIER_ACTIONS)
 export class QualifierActionsJobHandler extends JobHandler<Job> {
@@ -32,41 +33,50 @@ export class QualifierActionsJobHandler extends JobHandler<Job> {
     let nbFailuresQualification = 0
     let nombreActionsQualifiees = 0
 
+    let offset = 0
+    let actionsAQualifierSQL = []
+
     try {
-      const actionsInProgress = await ActionSqlModel.findAll({
-        where: actionsNonAnnuleesAvecDateEcheanceSuperieureA4Mois(maintenant)
-      })
+      do {
+        actionsAQualifierSQL = await ActionSqlModel.findAll({
+          where: actionsAQualifierAvecEcheancePlusDe4Mois(maintenant),
+          offset,
+          limit: MAX_ACTIONS,
+          order: [['dateCreation', 'ASC']]
+        })
+        offset = offset + MAX_ACTIONS
 
-      for (const actionSql of actionsInProgress) {
-        const action = ActionSqlRepository.actionFromSqlModel(actionSql)
-        let actionAQualifier: Action = action
+        for (const actionSql of actionsAQualifierSQL) {
+          const action = ActionSqlRepository.actionFromSqlModel(actionSql)
+          let actionAQualifier: Action = action
 
-        if (action.statut !== Action.Statut.TERMINEE) {
-          const updateResult = this.actionFactory.updateAction(action, {
-            idAction: action.id,
-            statut: Action.Statut.TERMINEE,
-            dateFinReelle: this.dateService.now()
-          })
-          if (isFailure(updateResult)) {
-            this.logger.warn(updateResult.error.message)
-            nbFailuresUpdate++
+          if (action.statut !== Action.Statut.TERMINEE) {
+            const updateResult = this.actionFactory.updateAction(action, {
+              idAction: action.id,
+              statut: Action.Statut.TERMINEE,
+              dateFinReelle: this.dateService.now()
+            })
+            if (isFailure(updateResult)) {
+              this.logger.warn(updateResult.error.message)
+              nbFailuresUpdate++
+              continue
+            }
+            actionAQualifier = updateResult?.data
+          }
+
+          const qualifierResult = Action.qualifier(
+            actionAQualifier,
+            Action.Qualification.Code.NON_SNP
+          )
+          if (isFailure(qualifierResult)) {
+            nbFailuresQualification++
+            this.logger.warn(qualifierResult.error.message)
             continue
           }
-          actionAQualifier = updateResult?.data
+          await this.actionRepository.save(qualifierResult.data)
+          nombreActionsQualifiees++
         }
-
-        const qualifierResult = Action.qualifier(
-          actionAQualifier,
-          Action.Qualification.Code.NON_SNP
-        )
-        if (isFailure(qualifierResult)) {
-          nbFailuresQualification++
-          this.logger.warn(qualifierResult.error.message)
-          continue
-        }
-        await this.actionRepository.save(qualifierResult.data)
-        nombreActionsQualifiees++
-      }
+      } while (actionsAQualifierSQL.length === MAX_ACTIONS)
     } catch (e) {
       this.logger.error(e.message)
       nbErreursCatchees++
@@ -87,8 +97,7 @@ export class QualifierActionsJobHandler extends JobHandler<Job> {
     }
   }
 }
-
-function actionsNonAnnuleesAvecDateEcheanceSuperieureA4Mois(
+function actionsAQualifierAvecEcheancePlusDe4Mois(
   maintenant: DateTime
 ): WhereOptions {
   return {
@@ -99,6 +108,11 @@ function actionsNonAnnuleesAvecDateEcheanceSuperieureA4Mois(
         Action.Statut.TERMINEE
       ]
     },
-    dateEcheance: { [Op.lt]: maintenant.minus({ months: 4 }).toJSDate() }
+    dateEcheance: {
+      [Op.lt]: maintenant.minus({ months: 4 }).toJSDate()
+    },
+    heuresQualifiees: {
+      [Op.eq]: null
+    }
   }
 }
