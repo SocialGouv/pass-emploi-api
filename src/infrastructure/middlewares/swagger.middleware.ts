88c3b320d323b50a13a21987647e5c3efd630081
Malek Zemni <malek.zemni@octo.com>
2024-05-16 16:22:01 +0200

    wip test swager

--- src/infrastructure/middlewares/swagger.middleware.ts
+++ src/infrastructure/middlewares/swagger.middleware.ts
@@ -13,15 +13,75 @@ export function useSwagger(
 ): void {
   const issuerUrl = appConfig.get('oidc.issuerUrl')
   const baserUrl = appConfig.get('baseUrl')
-  const oauth2SecuritySchemeObject: SecuritySchemeObject = {
+  const peConseiller: SecuritySchemeObject = {
     type: 'oauth2',
-    description: 'client_id: pass-emploi-swagger',
+    description: 'Conseiller France Travail',
     flows: {
-      implicit: {
-        authorizationUrl: `${issuerUrl}/protocol/openid-connect/auth`,
+      authorizationCode: {
+        authorizationUrl: `${issuerUrl}/protocol/openid-connect/auth?kc_idp_hint=pe-conseiller`,
         refreshUrl: `${issuerUrl}/protocol/openid-connect/token`,
         tokenUrl: `${issuerUrl}/protocol/openid-connect/token`,
-        scopes: {}
+        scopes: { openid: 'ok', email: 'oui', profile: '' }
+      }
+    }
+  }
+  const peJeune: SecuritySchemeObject = {
+    type: 'oauth2',
+    description: 'Conseiller France Travail',
+    flows: {
+      authorizationCode: {
+        authorizationUrl: `${issuerUrl}/protocol/openid-connect/auth?kc_idp_hint=pe-jeune`,
+        refreshUrl: `${issuerUrl}/protocol/openid-connect/token`,
+        tokenUrl: `${issuerUrl}/protocol/openid-connect/token`,
+        scopes: { openid: 'ok', email: 'oui', profile: '' }
+      }
+    }
+  }
+  const peConseillerBRSA: SecuritySchemeObject = {
+    type: 'oauth2',
+    description: 'Conseiller France Travail',
+    flows: {
+      authorizationCode: {
+        authorizationUrl: `${issuerUrl}/protocol/openid-connect/auth?kc_idp_hint=pe-brsa-conseiller`,
+        refreshUrl: `${issuerUrl}/protocol/openid-connect/token`,
+        tokenUrl: `${issuerUrl}/protocol/openid-connect/token`,
+        scopes: { openid: 'ok', email: 'oui', profile: '' }
+      }
+    }
+  }
+  const peJeuneBRSA: SecuritySchemeObject = {
+    type: 'oauth2',
+    description: 'Conseiller France Travail',
+    flows: {
+      authorizationCode: {
+        authorizationUrl: `${issuerUrl}/protocol/openid-connect/auth?kc_idp_hint=pe-brsa-jeune`,
+        refreshUrl: `${issuerUrl}/protocol/openid-connect/token`,
+        tokenUrl: `${issuerUrl}/protocol/openid-connect/token`,
+        scopes: { openid: 'ok', email: 'oui', profile: '' }
+      }
+    }
+  }
+  const miloConseiller: SecuritySchemeObject = {
+    type: 'oauth2',
+    description: 'Conseiller France Travail',
+    flows: {
+      authorizationCode: {
+        authorizationUrl: `${issuerUrl}/protocol/openid-connect/auth?kc_idp_hint=similo-conseiller`,
+        refreshUrl: `${issuerUrl}/protocol/openid-connect/token`,
+        tokenUrl: `${issuerUrl}/protocol/openid-connect/token`,
+        scopes: { openid: 'ok', email: 'oui', profile: '' }
+      }
+    }
+  }
+  const miloJeune: SecuritySchemeObject = {
+    type: 'oauth2',
+    description: 'Conseiller France Travail',
+    flows: {
+      authorizationCode: {
+        authorizationUrl: `${issuerUrl}/protocol/openid-connect/auth?kc_idp_hint=similo-jeune`,
+        refreshUrl: `${issuerUrl}/protocol/openid-connect/token`,
+        tokenUrl: `${issuerUrl}/protocol/openid-connect/token`,
+        scopes: { openid: 'ok', email: 'oui', profile: '' }
       }
     }
   }
@@ -33,14 +93,28 @@ export function useSwagger(
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Pass Emploi Api')
     .setVersion('1.0')
-    .addOAuth2(oauth2SecuritySchemeObject)
+    .addOAuth2(peConseiller)
+    .addOAuth2(peConseillerBRSA)
+    .addOAuth2(peJeune)
+    .addOAuth2(peJeuneBRSA)
+    .addOAuth2(miloJeune)
+    .addOAuth2(miloConseiller)
     .addSecurity('api_key', apiKeySecuritySchemeObject)
     .build()
   const document = SwaggerModule.createDocument(app, swaggerConfig)
+
   const customOptions: SwaggerCustomOptions = {
     swaggerOptions: {
       persistAuthorization: true,
-      oauth2RedirectUrl: `${baserUrl}/documentation/oauth2-redirect.html`
+      oauth2RedirectUrl: `${baserUrl}/documentation/oauth2-redirect.html`,
+      initOAuth: {
+        clientId: 'pass-emploi-swagger',
+        clientSecret: 'jopa',
+        scopes: ['openid', 'email', 'profile'],
+        scopeSeparator: ' ',
+        usePkceWithAuthorizationCodeGrant: true,
+        useBasicAuthenticationWithAccessCodeGrant: true
+      }
     }
   }
   SwaggerModule.setup('documentation', app, document, customOptions)
