e75ed72ea54a9b166454a5b6091638b737a78478
Bertrand de Maupeou <bertrand.de-maupeou-d-ableiges@hec.edu>
2024-08-27 11:26:53 +0200

    feat/ ajout vue engagement national

--- src/application/jobs/analytics/vues/3-0-migrate-schema.ts
+++ src/application/jobs/analytics/vues/3-0-migrate-schema.ts
@@ -38,6 +38,20 @@ export async function migrate(connexion: Sequelize): Promise<void> {
   `)
 
   await connexion.query(`
+    CREATE TABLE IF NOT EXISTS analytics_engagement_national
+    (
+        semaine                                             date,
+        structure                                           varchar,
+        type_utilisateur                                    varchar,
+        nombre_utilisateurs_2_mois                          integer,
+        nombre_utilisateurs_1_mois                          integer,
+        nombre_utilisateurs_engages_2_jours_dans_la_semaine integer,
+        nb_actifs_3_semaines_sur_6                          integer,
+        nb_actifs_4_semaines_sur_6                          integer
+    );
+`)
+
+  await connexion.query(`
     create index if not exists analytics_fonctionnalites_semaine_index on analytics_fonctionnalites (semaine);
     create index if not exists analytics_fonctionnalites_structure_index on analytics_fonctionnalites (structure);
     create index if not exists analytics_fonctionnalites_type_utilisateur_index on analytics_fonctionnalites (type_utilisateur);
@@ -49,5 +63,8 @@ export async function migrate(connexion: Sequelize): Promise<void> {
     create index if not exists analytics_engagement_type_utilisateur_index on analytics_engagement (type_utilisateur);
     create index if not exists analytics_engagement_region_index on analytics_engagement (region);
     create index if not exists analytics_engagement_departement_index on analytics_engagement (departement);
+    create index if not exists analytics_engagement_national_semaine_index on analytics_engagement_national (semaine);
+    create index if not exists analytics_engagement_national_structure_index on analytics_engagement_national (structure);
+    create index if not exists analytics_engagement_national_type_utilisateur_index on analytics_engagement_national (type_utilisateur);
   `)
 }
