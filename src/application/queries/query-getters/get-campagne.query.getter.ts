79919b56a91d0ce9a9efc0273430c4aa4e940d54
Malek Zemni <malek.zemni@octo.com>
2024-10-01 10:41:40 +0200

    feat: change wording campagne

--- src/application/queries/query-getters/get-campagne.query.getter.ts
+++ src/application/queries/query-getters/get-campagne.query.getter.ts
@@ -140,7 +140,7 @@ export const questionsInMemory = (): QuestionCampagneQueryModel[] => [
   {
     id: 4,
     libelle:
-      'Recommanderiez-vous l’application à vos proches ? Sur une échelle de 0 à 10, 0 = pas du tout et 10 = très certainement',
+      'Recommanderiez-vous l’application ? Sur une échelle de 0 à 10, 0 = pas du tout et 10 = très certainement',
     pourquoi: true,
     libellePourquoi:
       'Que pouvons-nous améliorer pour vous satisfaire davantage ?',
