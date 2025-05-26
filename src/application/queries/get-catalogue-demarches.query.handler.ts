5159aaa2df9ab0a8622d8ea17b32ee6b53f37421
Mzem <malek.zemni@octo.com>
2025-05-26 21:22:56 +0200

    feat: new catalogue demarches + forcer comment

--- src/application/queries/get-catalogue-demarches.query.handler.ts
+++ src/application/queries/get-catalogue-demarches.query.handler.ts
@@ -1,23 +1,13 @@
-import { Inject, Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { Query } from 'src/building-blocks/types/query'
-import { OidcClient } from 'src/infrastructure/clients/oidc-client.db'
-import {
-  PoleEmploiPartenaireClient,
-  PoleEmploiPartenaireClientToken
-} from 'src/infrastructure/clients/pole-emploi-partenaire-client.db'
-import { buildError } from 'src/utils/logger.module'
 import { QueryHandler } from '../../building-blocks/types/query-handler'
-import { Result, isFailure } from '../../building-blocks/types/result'
+import { Result } from '../../building-blocks/types/result'
 import { Authentification } from '../../domain/authentification'
 import { Core, beneficiaireEstFTConnect } from '../../domain/core'
 import { JeuneAuthorizer } from '../authorizers/jeune-authorizer'
 import { ThematiqueQueryModel } from './query-models/catalogue.query-model'
 
-import {
-  codeCommentDemarchesCachees,
-  codeQuoiDemarchesCachees
-} from 'src/infrastructure/clients/utils/demarche-liste-visible'
-import { TypesDemarcheQueryModel } from './query-models/types-demarche.query-model'
+import { catalogueDemarchesInMemory } from 'src/infrastructure/clients/utils/demarches-in-memory'
 
 export interface GetCatalogueDemarchesQuery extends Query {
   accessToken: string
@@ -29,69 +19,22 @@ export class GetCatalogueDemarchesQueryHandler extends QueryHandler<
   GetCatalogueDemarchesQuery,
   ThematiqueQueryModel[]
 > {
-  constructor(
-    @Inject(PoleEmploiPartenaireClientToken)
-    private poleEmploiPartenaireClient: PoleEmploiPartenaireClient,
-    private readonly jeuneAuthorizer: JeuneAuthorizer,
-    private oidcClient: OidcClient
-  ) {
+  constructor(private readonly jeuneAuthorizer: JeuneAuthorizer) {
     super('GetCatalogueQueryHandler')
   }
 
   async handle(
-    query: GetCatalogueDemarchesQuery
+    _query: GetCatalogueDemarchesQuery
   ): Promise<ThematiqueQueryModel[]> {
-    const idpToken = await this.oidcClient.exchangeTokenJeune(
-      query.accessToken,
-      query.structure
-    )
-    const catalogueResult = await this.poleEmploiPartenaireClient.getCatalogue(
-      idpToken
-    )
-
-    if (isFailure(catalogueResult)) {
-      this.logger.error(
-        buildError(
-          `Impossible de récupérer le catalogue de démarches depuis PE`,
-          Error(catalogueResult.error.message)
-        )
-      )
-      return []
-    }
-
-    return catalogueResult.data.map(pourquoi => {
-      const codePourquoi = pourquoi.code
-      const libellePourquoi = pourquoi.libelle
-      const listeDemarches: TypesDemarcheQueryModel[] = []
-      pourquoi.typesDemarcheRetourEmploi.forEach(quoi => {
-        if (!codeQuoiDemarchesCachees.has(quoi.code)) {
-          const listeDemarchesVisible = quoi.moyensRetourEmploi.filter(
-            comment =>
-              !codeCommentDemarchesCachees.has(comment.code) &&
-              comment.droitCreation
-          )
-          if (listeDemarchesVisible.length > 0) {
-            listeDemarches.push({
-              codePourquoi,
-              libellePourquoi,
-              codeQuoi: quoi.code,
-              libelleQuoi: quoi.libelle,
-              commentObligatoire: quoi.moyensRetourEmploi.length > 0,
-              comment: listeDemarchesVisible.map(comment => {
-                return {
-                  code: comment.code,
-                  label: comment.libelle
-                }
-              })
-            })
-          }
-        }
-      })
-
+    return catalogueDemarchesInMemory.map(thematique => {
       return {
-        code: codePourquoi,
-        libelle: libellePourquoi,
-        demarches: listeDemarches
+        code: thematique.code,
+        libelle: thematique.libelle,
+        demarches: thematique.demarches.map(demarche => ({
+          ...demarche,
+          commentObligatoire: false,
+          comment: []
+        }))
       }
     })
   }
